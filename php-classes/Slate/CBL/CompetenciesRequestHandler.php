e029f2bc11fdc2c12ab5157252b5a7f9ed87f7ba
Chris Alfano <chris@slate.is>
2014-12-01 00:35:52 -0500

    implemented competency averages export

--- php-classes/Slate/CBL/CompetenciesRequestHandler.php
+++ php-classes/Slate/CBL/CompetenciesRequestHandler.php
@@ -3,12 +3,25 @@
 namespace Slate\CBL;
 
 use DB;
+use SpreadsheetWriter;
 use TableNotFoundException;
+use Slate\People\Student;
 
 class CompetenciesRequestHandler extends \RecordsRequestHandler
 {
     public static $recordClass = Competency::class;
     public static $browseOrder = 'ContentAreaID, Code';
+
+
+    public static function handleRecordsRequest($action = null)
+    {
+        switch ($action ?: $action = static::shiftPath()) {
+            case 'export':
+                return static::handleExportRequest();
+            default:
+                return parent::handleRecordsRequest($action);
+        }
+    }
 
     public static function handleRecordRequest(\ActiveRecord $Competency, $action = false)
     {
@@ -19,7 +32,122 @@ class CompetenciesRequestHandler extends \RecordsRequestHandler
                 return parent::handleRecordRequest($Competency, $action);
         }
     }
-    
+
+    public static function handleExportRequest()
+    {
+        $sw = new SpreadsheetWriter();
+
+        // fetch key objects from database
+        $students = Student::getAllByListIdentifier(empty($_GET['students']) ? 'all' : $_GET['students']);
+        $contentAreas = ContentArea::getAll(['order' => 'Code']);
+
+
+        // collect counts of all missing demonstrations by student+competency
+        try {
+            $missingResults = DB::allRecords(
+                'SELECT StudentID, CompetencyID, SUM(neededDemonstrationsMissed) AS totalNeededDemonstrationsMissed'
+                .' FROM ('
+                .'  SELECT'
+                .'    Demonstration.StudentID'
+                .'    ,Skill.CompetencyID'
+#                .'    ,DemonstrationSkill.SkillID'
+#                .'    ,Skill.DemonstrationsRequired'
+#                .'    ,SUM(IF(DemonstrationSkill.Level = 0, 1, 0)) totalMissing'
+#                .'    ,SUM(IF(DemonstrationSkill.Level != 0, 1, 0)) totalNotMissing'
+#                .'    ,Skill.DemonstrationsRequired'
+                .'    ,LEAST('
+                .'       GREATEST(Skill.DemonstrationsRequired - SUM(IF(DemonstrationSkill.Level != 0, 1, 0)), 0)' // how many needed demonstrations don't have non-missing levels
+                .'       ,SUM(IF(DemonstrationSkill.Level = 0, 1, 0))' // total missing demonstrations for this skill
+                .'    ) AS neededDemonstrationsMissed'
+                .'   FROM `%s` Demonstration'
+                .'   JOIN `%s` DemonstrationSkill'
+                .'    ON DemonstrationSkill.DemonstrationID = Demonstration.ID'
+                .'   JOIN `%s` Skill'
+                .'    ON Skill.ID = DemonstrationSkill.SkillID'
+                .'   WHERE Demonstration.StudentID IN (%s)'
+                .'   GROUP BY DemonstrationSkill.SkillID'
+                .' ) MissingDemonstrationsByStudentSkill'
+                .' GROUP BY StudentID, CompetencyID'
+                ,[
+                    Demonstration::$tableName
+                    ,DemonstrationSkill::$tableName
+                    ,Skill::$tableName
+                    ,implode(',', array_map(function($Student) {
+                        return $Student->ID;
+                    }, $students))
+                ]
+            );
+
+            $missingDemonstrationsByStudentCompetency = [];
+            foreach ($missingResults AS $result) {
+                $missingDemonstrationsByStudentCompetency[$result['StudentID']][$result['CompetencyID']] = intval($result['totalNeededDemonstrationsMissed']);
+            }
+        } catch (TableNotFoundException $e) {
+            $missingDemonstrationsByStudentCompetency = [];
+        }
+
+
+        // build and output headers list
+        $headers = [
+            'Student Name',
+            'Student Number',
+            'Grade Level'
+        ];
+
+        foreach ($contentAreas AS $ContentArea) {
+            foreach ($ContentArea->Competencies AS $Competency) {
+                $headers[] = $Competency->Code . '-AVG';
+            }
+
+            $headers[] = $ContentArea->Code . '-Logged';
+            $headers[] = $ContentArea->Code . '-Total';
+            $headers[] = $ContentArea->Code . '-Missing';
+            $headers[] = $ContentArea->Code . '-AVG';
+        }
+
+        $sw->writeRow($headers);
+
+
+        // one row for each demonstration
+        foreach ($students AS $Student) {
+            $row = [
+                $Student->FullName,
+                $Student->StudentNumber,
+                9 // TODO: don't hardcode
+            ];
+
+            foreach ($contentAreas AS $ContentArea) {
+                $demonstrationsCounted = 0;
+                $demonstrationsRequired = 0;
+                $demonstrationsMissing = 0;
+                $contentAreaAverageSamples = [];
+
+                foreach ($ContentArea->Competencies AS $Competency) {
+                    $competencyCompletion = $Competency->getCompletionForStudent($Student);
+
+                    $row[] = round($competencyCompletion['demonstrationsAverage'], 2);
+                    $demonstrationsCounted += $competencyCompletion['demonstrationsCount'];
+                    $demonstrationsRequired += $Competency->getTotalDemonstrationsRequired();
+
+                    if ($competencyCompletion['demonstrationsCount']) {
+                        $contentAreaAverageSamples[] = $competencyCompletion['demonstrationsAverage'];
+                    }
+
+                    if(isset($missingDemonstrationsByStudentCompetency[$Student->ID][$Competency->ID])) {
+                        $demonstrationsMissing += $missingDemonstrationsByStudentCompetency[$Student->ID][$Competency->ID];
+                    }
+                }
+
+                $row[] = $demonstrationsCounted;
+                $row[] = $demonstrationsRequired;
+                $row[] = $demonstrationsMissing;
+                $row[] = count($contentAreaAverageSamples) ? round(array_sum($contentAreaAverageSamples) / count($contentAreaAverageSamples), 2) : 0;
+            }
+
+            $sw->writeRow($row);
+        }
+    }
+
     public static function handleDemonstrationsRequest(Competency $Competency)
     {
         // get input students
@@ -28,7 +156,7 @@ class CompetenciesRequestHandler extends \RecordsRequestHandler
         }
 
         $students = is_string($_REQUEST['students']) ? explode(',', $_REQUEST['students']) : $_REQUEST['students'];
-        
+
         foreach ($students AS $studentId) {
             if (!ctype_digit($studentId)) {
                 return static::throwInvalidRequestError('competency required');
@@ -55,7 +183,7 @@ class CompetenciesRequestHandler extends \RecordsRequestHandler
         } catch (TableNotFoundException $e) {
             $skillDemonstrations = [];
         }
-        
+
         // cast strings to integers
         foreach ($skillDemonstrations AS &$skillDemonstration) {
             $skillDemonstration['DemonstrationID'] = intval($skillDemonstration['DemonstrationID']);
