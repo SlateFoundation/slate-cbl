d40670c179f0dfdcac818871975eb8dddfdc70ea
Chris Alfano <chris@slate.is>
2015-02-16 14:27:06 -0500

    restored deleted inherited files

---
+++ sencha-workspace/packages/slate-cbl/src/model/Demonstration.js
@@ -0,0 +1,88 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('Slate.cbl.model.Demonstration', {
+    extend: 'Ext.data.Model',
+    requires: [
+        'Slate.cbl.proxy.Records',
+        'Ext.data.identifier.Negative'
+    ],
+
+
+    // model config
+    idProperty: 'ID',
+    identifier: 'negative',
+
+    fields: [
+        {
+            name: "ID",
+            type: "int",
+            useNull: true
+        },
+        {
+            name: "Class",
+            type: "string",
+            defaultValue: "Slate\\CBL\\Demonstration"
+        },
+        {
+            name: "Created",
+            type: "date",
+            dateFormat: "timestamp",
+            useNull: true
+        },
+        {
+            name: "CreatorID",
+            type: "int",
+            useNull: true
+        },
+        {
+            name: "StudentID",
+            type: "int"
+        },
+//        {
+//            name: "SkillID",
+//            type: "int"
+//        },
+        {
+            name: "Demonstrated",
+            type: "date",
+            dateFormat: "Y-m-d"
+        },
+//        {
+//            name: "Level",
+//            type: "string"
+//        },
+        {
+            name: "ExperienceType",
+            type: "string"
+        },
+        {
+            name: "Context",
+            type: "string"
+        },
+        {
+            name: "PerformanceType",
+            type: "string"
+        },
+        {
+            name: "ArtifactURL",
+            type: "string",
+            useNull: true
+        },
+        {
+            name: "Comments",
+            type: "string",
+            useNull: true
+        },
+
+        // server-provided metadata
+        {
+            name: 'competencyCompletions',
+            defaultValue: [],
+            persist: false
+        }
+    ],
+
+    proxy: {
+        type: 'slate-cbl-records',
+        url: '/cbl/demonstrations'
+    }
+});
