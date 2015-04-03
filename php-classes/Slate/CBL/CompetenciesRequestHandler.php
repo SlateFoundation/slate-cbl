c08b67964b411a6ba53c872d801592d196fc7419
Jeffrey Mealo <jmealo@jarv.us>
2015-04-03 12:36:17 -0400

    disable diagnostic logging during competencies export

--- php-classes/Slate/CBL/CompetenciesRequestHandler.php
+++ php-classes/Slate/CBL/CompetenciesRequestHandler.php
@@ -37,6 +37,9 @@ class CompetenciesRequestHandler extends \RecordsRequestHandler
     {
         $GLOBALS['Session']->requireAccountLevel('Staff');
         
+        // This was causing a script timeout (30 seconds), this should help speed it up
+        \Site::$debug = false;
+        
         $sw = new SpreadsheetWriter();
 
         // fetch key objects from database
