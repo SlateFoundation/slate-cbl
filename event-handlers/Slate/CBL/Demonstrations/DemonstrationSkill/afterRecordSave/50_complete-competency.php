49860c9543b6d0e1d3d3105e29dc762e5e02ba52
Chris Alfano <chris@jarv.us>
2015-05-29 11:45:53 -0400

    enable students to graduate levels with only overrides

--- event-handlers/Slate/CBL/Demonstrations/DemonstrationSkill/afterRecordSave/50_complete-competency.php
+++ event-handlers/Slate/CBL/Demonstrations/DemonstrationSkill/afterRecordSave/50_complete-competency.php
@@ -8,10 +8,13 @@ $Competency = $_EVENT['Record']->Skill->Competency;
 $completion = $Competency->getCompletionForStudent($Student);
 
 if (
-    $completion['demonstrationsComplete'] >= $Competency->getTotalDemonstrationsRequired() &&
-    $completion['demonstrationsAverage'] >= ($completion['currentLevel'] + $Competency->getMinimumAverageOffset()) &&
-    $completion['currentLevel'] < $Competency->getMaximumTargetLevel()
-    ) {
+        $completion['demonstrationsComplete'] >= $Competency->getTotalDemonstrationsRequired() &&
+        (
+            $completion['demonstrationsLogged'] == 0 || // if demonstrationsComplete is full but none are logged, the student has fulfilled all their demonstrations via overrides and the average is irrelevant
+            $completion['demonstrationsAverage'] >= ($completion['currentLevel'] + $Competency->getMinimumAverageOffset())
+        ) &&
+        $completion['currentLevel'] < $Competency->getMaximumTargetLevel()
+   ) {
 
     // enroll student in next level
     StudentCompetency::create([
