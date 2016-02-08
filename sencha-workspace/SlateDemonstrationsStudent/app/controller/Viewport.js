aca3f3282fa69d3f2cb6e847bef272b924880e51
Chris Alfano <chris@jarv.us>
2016-02-07 21:34:35 -0500

    Render student recent progress and demonstrations dashboard

--- sencha-workspace/SlateDemonstrationsStudent/app/controller/Viewport.js
+++ sencha-workspace/SlateDemonstrationsStudent/app/controller/Viewport.js
@@ -1,22 +1,64 @@
 Ext.define('SlateDemonstrationsStudent.controller.Viewport', {
     extend: 'Ext.app.Controller',
+    requires: [
+        'Slate.API'
+    ],
+
 
     views: [
-        'main.Main'
+        'Dashboard@Slate.cbl.view.student',
+        'RecentProgress@Slate.cbl.view.student'
     ],
 
     config: {
         refs: {
-            mainView: {
-                selector: 'app-main',
+            dashboardCt: {
+                selector: 'slate-cbl-student-dashboard',
                 autoCreate: true,
 
-                xtype: 'app-main'
+                xtype: 'slate-cbl-student-dashboard'
+            },
+            recentProgressCmp: {
+                selector: 'slate-cbl-student-recentprogress',
+                autoCreate: true,
+
+                xtype: 'slate-cbl-student-recentprogress'
             }
         },
     },
 
     onLaunch: function () {
-        this.getMainView().render('slateapp-viewport');
+        var siteEnv = window.SiteEnvironment || {},
+            cblStudentId = (siteEnv.cblStudent || {}).ID,
+            cblContentArea = siteEnv.cblContentArea || null,
+            recentProgressCmp, dashboardCt;
+
+        // fetch component instances
+        recentProgressCmp = this.getRecentProgressCmp();
+        dashboardCt = this.getDashboardCt();
+
+        // configure recent progress component with any available embedded data
+        if (cblStudentId) {
+            recentProgressCmp.setStudentId(cblStudentId);
+        }
+
+        if (cblContentArea) {
+            recentProgressCmp.setContentAreaId(cblContentArea);
+        }
+
+        // configure dashboard with any available embedded data
+        if (cblStudentId) {
+            dashboardCt.setStudentId(cblStudentId);
+        }
+
+        if (siteEnv.cblCompetencies) {
+            dashboardCt.getCompetenciesStore().loadData(siteEnv.cblCompetencies);
+        }
+
+        // render components
+        Ext.suspendLayouts();
+        recentProgressCmp.render('slateapp-viewport');
+        dashboardCt.render('slateapp-viewport');
+        Ext.resumeLayouts(true);
     }
 });
