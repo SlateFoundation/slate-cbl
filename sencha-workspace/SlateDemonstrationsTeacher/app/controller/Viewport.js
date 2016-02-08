0fcb95b9d6ae6d0e79b461f2dd7b6e49d53b6779
Chris Alfano <chris@jarv.us>
2016-02-07 20:16:47 -0500

    Render cbl teacher demonstrations dashboard

--- sencha-workspace/SlateDemonstrationsTeacher/app/controller/Viewport.js
+++ sencha-workspace/SlateDemonstrationsTeacher/app/controller/Viewport.js
@@ -2,21 +2,42 @@ Ext.define('SlateDemonstrationsTeacher.controller.Viewport', {
     extend: 'Ext.app.Controller',
 
     views: [
-        'main.Main'
+        'Dashboard@Slate.cbl.view.teacher'
     ],
 
     config: {
         refs: {
-            mainView: {
-                selector: 'app-main',
+            dashboardCt: {
+                selector: 'slate-cbl-teacher-dashboard',
                 autoCreate: true,
 
-                xtype: 'app-main'
+                xtype: 'slate-cbl-teacher-dashboard'
             }
         },
     },
 
     onLaunch: function () {
-        this.getMainView().render('slateapp-viewport');
+        var siteEnv = window.SiteEnvironment || {},
+            contentAreaCode = (siteEnv.cblContentArea || {}).Code,
+            dashboardCt, progressGrid;
+
+        dashboardCt = this.getDashboardCt({});
+        progressGrid = dashboardCt.getProgressGrid();
+
+        // configure dashboard with any available embedded data
+        if (contentAreaCode) {
+            progressGrid.setStudentDashboardLink('/cbl/student-dashboard?content-area=' + escape(contentAreaCode));
+        }
+
+        if (siteEnv.cblStudents) {
+            progressGrid.getStudentsStore().loadData(siteEnv.cblStudents);
+        }
+
+        if (siteEnv.cblCompetencies) {
+            progressGrid.getCompetenciesStore().loadData(siteEnv.cblCompetencies);
+        }
+
+        // render dashboard
+        dashboardCt.render('slateapp-viewport');
     }
 });
