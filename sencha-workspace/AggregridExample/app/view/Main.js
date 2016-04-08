5b4a1a71a9eb0b7597efc392ce840d979c9aa13d
Chris Alfano <chris@jarv.us>
2016-04-08 15:05:07 -0400

    Configure columnsStore and rowsStore

--- sencha-workspace/AggregridExample/app/view/Main.js
+++ sencha-workspace/AggregridExample/app/view/Main.js
@@ -1,4 +1,30 @@
 Ext.define('AggregridExample.view.Main', {
     extend: 'Jarvus.aggregrid.Aggregrid',
-    xtype: 'app-main'
+    xtype: 'app-main',
+
+
+    config: {
+        columnsStore: {
+            //type: 'store',
+            fields: ['id', 'name'],
+            data: [
+                {id: 1, name: 'Ali'},
+                {id: 2, name: 'Chris'},
+                {id: 3, name: 'Ryon'},
+                {id: 4, name: 'Kevin'},
+                {id: 5, name: 'Christian'}
+            ]
+        },
+        rowsStore: {
+            //type: 'store',
+            fields: ['id', 'title'],
+            data: [
+                {id: 1, title: 'Task 1'},
+                {id: 2, title: 'Task 2'},
+                {id: 3, title: 'Task 3'},
+                {id: 4, title: 'Task 4'}
+            ]
+        },
+
+    }
 });
