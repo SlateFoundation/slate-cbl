827c1d11ba57d90c3aaeaba61ee1e4cdd50b3574
aliyeysides <ali.yeysides@gmail.com>
2016-04-12 22:00:50 -0400

    Remove commented code & add extJS to preload

--- sencha-workspace/tests/index.js
+++ sencha-workspace/tests/index.js
@@ -6,16 +6,14 @@ harness.configure({
     preload     : [
         // version of ExtJS used by your application 
         // (not needed if you use Sencha Cmd which builds a complete 'all-file' including Ext JS itself)
-        // 'ext-all.css',
-        // 'ext-all-debug.js',
+        '../ext-6.0.1/build/ext-all.js',
+        '../ext-6.0.1/build/ext-debug.js',
 
         // Your application files
-        // '../resources/yourproject-css-all.css',
         '../build/production/AggregridExample/app.js'
     ]
 });
 
 harness.start(
     'siesta-4.0.6-lite/tests/sanity/010_sanity.t.js'
-    // 'siesta-4.0.6-lite/tests/sanity/020_basic.t.js'
 );
