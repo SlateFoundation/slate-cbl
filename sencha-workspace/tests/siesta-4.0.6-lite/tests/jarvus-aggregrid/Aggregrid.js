40f815939efc8a478f52e67aa8a0760d9fd74adb
aliyeysides <ali.yeysides@gmail.com>
2016-04-13 05:39:19 -0400

    Created new tests folder for jarvus-aggregrid component and included Aggregrid.js test file

---
+++ sencha-workspace/tests/siesta-4.0.6-lite/tests/jarvus-aggregrid/Aggregrid.js
@@ -0,0 +1,3 @@
+StartTest(function(t){
+	// TODO: create series of tests that check that rowsStore and columnsStore return correct counts and the update event has been fired exactly once with every combination of instantiation, store configuration, and rendering happeing in different orders and groupings
+})
