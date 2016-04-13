e5d75bea7b71f90d84b0389431ff87a148a08d37
aliyeysides <ali.yeysides@gmail.com>
2016-04-13 05:28:44 -0400

    Modify sanity test to include check for MyAggregrid view class

--- sencha-workspace/tests/siesta-4.0.6-lite/tests/sanity/010_sanity.t.js
+++ sencha-workspace/tests/siesta-4.0.6-lite/tests/sanity/010_sanity.t.js
@@ -3,11 +3,12 @@ StartTest(function(t) {
     t.diag("Sanity");
 
     t.ok(Ext, 'ExtJS is here');
-    t.ok(Ext.Window, '.. indeed');
-
 
     t.ok(AggregridExample.Application, 'AggregridExample is here');
-    t.ok(AggregridExample.Application, '.. indeed');
+
+    t.ok(AggregridExample.view.MyAggregrid, 'AggregridExample view is here');
+
+    // AggregridExample.view.MyAggregrid
 
     t.done();   // Optional, marks the correct exit point from the test
 })
