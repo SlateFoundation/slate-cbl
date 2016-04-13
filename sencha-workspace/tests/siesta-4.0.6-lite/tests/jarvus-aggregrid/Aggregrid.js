81e59b452eb85b5d7abe13ae3772266f8e14a473
aliyeysides <ali.yeysides@gmail.com>
2016-04-13 07:32:26 -0400

    Wrote tests asserting rowsStore and columnsStore entry count

--- sencha-workspace/tests/siesta-4.0.6-lite/tests/jarvus-aggregrid/Aggregrid.js
+++ sencha-workspace/tests/siesta-4.0.6-lite/tests/jarvus-aggregrid/Aggregrid.js
@@ -1,3 +1,59 @@
 StartTest(function(t){
 	// TODO: create series of tests that check that rowsStore and columnsStore return correct counts and the update event has been fired exactly once with every combination of instantiation, store configuration, and rendering happeing in different orders and groupings
+
+		t.requireOk('Jarvus.aggregrid.Aggregrid', function () {
+
+			var AggregridView = Ext.create('Jarvus.aggregrid.Aggregrid', {
+				renderTo: Ext.getBody(),
+        columnsStore: {
+            //type: 'store',
+            fields: ['id', 'fullName'],
+            data: [
+                {id: 1, fullName: 'Ali', rank: 'Pro'},
+                {id: 2, fullName: 'Chris'},
+                {id: 3, fullName: 'Ryon'},
+                {id: 4, fullName: 'Kevin'},
+                {id: 5, fullName: 'Christian'}
+            ]
+        },
+        columnHeaderTpl: [
+            '{fullName}',
+            '<tpl if="rank">',
+                ' ({rank})',
+            '</tpl>'
+        ],
+
+        rowsStore: {
+            //type: 'store',
+            fields: ['id', 'taskName'],
+            data: [
+                {id: 1, taskName: 'Task 1'},
+                {id: 2, taskName: 'Task 2'},
+                {
+                    id: 3, taskName: 'Task 3',
+                    rows: [
+                        { taskName: 'Task 3 - Subtask 1'},
+                        { taskName: 'Task 3 - Subtask 2'},
+                        { taskName: 'Task 3 - Subtask 3'}
+                    ]
+                },
+                {id: 4, taskName: 'Task 4'}
+            ]
+        },
+        rowHeaderField: 'taskName'
+		});
+			
+			console.log('##View', AggregridView);
+
+			t.waitForComponentVisible(AggregridView, function() {
+				t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
+				t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
+				// t.is(AggregridView.columnsStore.getCount(), 'Rendered all data in columnStore OK');
+				// t.is(AggregridView.columnsStore.getCount(), 'Rendered all data in columnStore OK');
+
+				// t.matchGridCellContent(grid, 0, 0, grid.store.first().getFullName(), 'Found full name in first cell');
+			});
+
+			// t.is(AggregridView.getRowsStore(), 'Slack Jocum', 'getFullName works ok');
+		});
 })
