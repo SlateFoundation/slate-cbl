96f14053dfe9062704e08e64822d9e99a44a6ff0
aliyeysides <ali.yeysides@gmail.com>
2016-04-14 09:17:04 -0400

    Write tests to make sure 'refresh' event fires appropriately

--- sencha-workspace/tests/siesta-4.0.6-lite/tests/jarvus-aggregrid/Aggregrid.js
+++ sencha-workspace/tests/siesta-4.0.6-lite/tests/jarvus-aggregrid/Aggregrid.js
@@ -5,55 +5,63 @@ StartTest(function(t){
 
 			var AggregridView = Ext.create('Jarvus.aggregrid.Aggregrid', {
 				renderTo: Ext.getBody(),
-        columnsStore: {
-            //type: 'store',
-            fields: ['id', 'fullName'],
-            data: [
-                {id: 1, fullName: 'Ali', rank: 'Pro'},
-                {id: 2, fullName: 'Chris'},
-                {id: 3, fullName: 'Ryon'},
-                {id: 4, fullName: 'Kevin'},
-                {id: 5, fullName: 'Christian'}
-            ]
-        },
-        columnHeaderTpl: [
-            '{fullName}',
-            '<tpl if="rank">',
-                ' ({rank})',
-            '</tpl>'
-        ],
+		    columnsStore: {
+		        //type: 'store',
+		        fields: ['id', 'fullName'],
+		        data: [
+		            {id: 1, fullName: 'Ali', rank: 'Pro'},
+		            {id: 2, fullName: 'Chris'},
+		            {id: 3, fullName: 'Ryon'},
+		            {id: 4, fullName: 'Kevin'},
+		            {id: 5, fullName: 'Christian'}
+		        ]
+		    },
+		    columnHeaderTpl: [
+		        '{fullName}',
+		        '<tpl if="rank">',
+		            ' ({rank})',
+		        '</tpl>'
+		    ],
 
-        rowsStore: {
-            //type: 'store',
-            fields: ['id', 'taskName'],
-            data: [
-                {id: 1, taskName: 'Task 1'},
-                {id: 2, taskName: 'Task 2'},
-                {
-                    id: 3, taskName: 'Task 3',
-                    rows: [
-                        { taskName: 'Task 3 - Subtask 1'},
-                        { taskName: 'Task 3 - Subtask 2'},
-                        { taskName: 'Task 3 - Subtask 3'}
-                    ]
-                },
-                {id: 4, taskName: 'Task 4'}
-            ]
-        },
-        rowHeaderField: 'taskName'
-		});
-			
-			console.log('##View', AggregridView);
-
-			t.waitForComponentVisible(AggregridView, function() {
-				t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
-				t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
-				// t.is(AggregridView.columnsStore.getCount(), 'Rendered all data in columnStore OK');
-				// t.is(AggregridView.columnsStore.getCount(), 'Rendered all data in columnStore OK');
-
-				// t.matchGridCellContent(grid, 0, 0, grid.store.first().getFullName(), 'Found full name in first cell');
+		    rowsStore: {
+		        //type: 'store',
+		        fields: ['id', 'taskName'],
+		        data: [
+		            {id: 1, taskName: 'Task 1'},
+		            {id: 2, taskName: 'Task 2'},
+		            {
+		                id: 3, taskName: 'Task 3',
+		                rows: [
+		                    { taskName: 'Task 3 - Subtask 1'},
+		                    { taskName: 'Task 3 - Subtask 2'},
+		                    { taskName: 'Task 3 - Subtask 3'}
+		                ]
+		            },
+		            {id: 4, taskName: 'Task 4'}
+		        ]
+		    },
+		    rowHeaderField: 'taskName'
 			});
 
-			// t.is(AggregridView.getRowsStore(), 'Slack Jocum', 'getFullName works ok');
+			t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateColumnsStore');
+
+			AggregridView.updateColumnsStore();
+
+			t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateRowsStore');
+
+			AggregridView.updateRowsStore();
+
+			
+			t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after component instantiation');
+
+
+			t.waitForComponentVisible(AggregridView, function() {
+
+				t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
+				t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
+
+			});
+
+
 		});
 })
