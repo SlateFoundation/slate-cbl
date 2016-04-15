fb5cf7b2220187939d00173e8bfe672c59d2de4f
Chris Alfano <chris@jarv.us>
2016-04-15 15:15:36 -0400

    Restore siesta 010_sanity.t.js example to stock

--- sencha-workspace/tests/siesta-4.0.6-lite/tests/sanity/010_sanity.t.js
+++ sencha-workspace/tests/siesta-4.0.6-lite/tests/sanity/010_sanity.t.js
@@ -3,12 +3,11 @@ StartTest(function(t) {
     t.diag("Sanity");
 
     t.ok(Ext, 'ExtJS is here');
+    t.ok(Ext.Window, '.. indeed');
 
-    t.ok(AggregridExample.Application, 'AggregridExample is here');
 
-    t.ok(AggregridExample.view.MyAggregrid, 'AggregridExample view is here');
-
-    // AggregridExample.view.MyAggregrid
+    t.ok(Your.Project, 'My project is here');
+    t.ok(Your.Project.Util, '.. indeed');
 
     t.done();   // Optional, marks the correct exit point from the test
-})
+})    
