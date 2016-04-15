0d3499ed78be7de4584cdeb79539a43cfe2a53ec
Chris Alfano <chris@jarv.us>
2016-04-15 15:18:58 -0400

    Clean up indentation and trailing whitespace

--- sencha-workspace/tests/jarvus-aggregrid/020_aggregrid.t.js
+++ sencha-workspace/tests/jarvus-aggregrid/020_aggregrid.t.js
@@ -1,67 +1,66 @@
 StartTest(function(t){
-	// TODO: create series of tests that check that rowsStore and columnsStore return correct counts and the update event has been fired exactly once with every combination of instantiation, store configuration, and rendering happeing in different orders and groupings
+    // TODO: create series of tests that check that rowsStore and columnsStore return correct counts and the update event has been fired exactly once with every combination of instantiation, store configuration, and rendering happeing in different orders and groupings
 
-		t.requireOk('Jarvus.aggregrid.Aggregrid', function () {
+    t.requireOk('Jarvus.aggregrid.Aggregrid', function () {
 
-			var AggregridView = Ext.create('Jarvus.aggregrid.Aggregrid', {
-				renderTo: Ext.getBody(),
-		    columnsStore: {
-		        //type: 'store',
-		        fields: ['id', 'fullName'],
-		        data: [
-		            {id: 1, fullName: 'Ali', rank: 'Pro'},
-		            {id: 2, fullName: 'Chris'},
-		            {id: 3, fullName: 'Ryon'},
-		            {id: 4, fullName: 'Kevin'},
-		            {id: 5, fullName: 'Christian'}
-		        ]
-		    },
-		    columnHeaderTpl: [
-		        '{fullName}',
-		        '<tpl if="rank">',
-		            ' ({rank})',
-		        '</tpl>'
-		    ],
+        var AggregridView = Ext.create('Jarvus.aggregrid.Aggregrid', {
+            renderTo: Ext.getBody(),
+            columnsStore: {
+                //type: 'store',
+                fields: ['id', 'fullName'],
+                data: [
+                    {id: 1, fullName: 'Ali', rank: 'Pro'},
+                    {id: 2, fullName: 'Chris'},
+                    {id: 3, fullName: 'Ryon'},
+                    {id: 4, fullName: 'Kevin'},
+                    {id: 5, fullName: 'Christian'}
+                ]
+            },
+            columnHeaderTpl: [
+                '{fullName}',
+                '<tpl if="rank">',
+                    ' ({rank})',
+                '</tpl>'
+            ],
 
-		    rowsStore: {
-		        //type: 'store',
-		        fields: ['id', 'taskName'],
-		        data: [
-		            {id: 1, taskName: 'Task 1'},
-		            {id: 2, taskName: 'Task 2'},
-		            {
-		                id: 3, taskName: 'Task 3',
-		                rows: [
-		                    { taskName: 'Task 3 - Subtask 1'},
-		                    { taskName: 'Task 3 - Subtask 2'},
-		                    { taskName: 'Task 3 - Subtask 3'}
-		                ]
-		            },
-		            {id: 4, taskName: 'Task 4'}
-		        ]
-		    },
-		    rowHeaderField: 'taskName'
-			});
+            rowsStore: {
+                //type: 'store',
+                fields: ['id', 'taskName'],
+                data: [
+                    {id: 1, taskName: 'Task 1'},
+                    {id: 2, taskName: 'Task 2'},
+                    {
+                        id: 3, taskName: 'Task 3',
+                        rows: [
+                            { taskName: 'Task 3 - Subtask 1'},
+                            { taskName: 'Task 3 - Subtask 2'},
+                            { taskName: 'Task 3 - Subtask 3'}
+                        ]
+                    },
+                    {id: 4, taskName: 'Task 4'}
+                ]
+            },
+            rowHeaderField: 'taskName'
+        });
 
-			t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateColumnsStore');
+        t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateColumnsStore');
 
-			AggregridView.updateColumnsStore();
+        AggregridView.updateColumnsStore();
 
-			t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateRowsStore');
+        t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateRowsStore');
 
-			AggregridView.updateRowsStore();
-
-			
-			t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after component instantiation');
+        AggregridView.updateRowsStore();
 
 
-			t.waitForComponentVisible(AggregridView, function() {
-
-				t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
-				t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
-
-			});
+        t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after component instantiation');
 
 
-		});
+        t.waitForComponentVisible(AggregridView, function() {
+
+            t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
+            t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
+
+        });
+
+    });
 })
