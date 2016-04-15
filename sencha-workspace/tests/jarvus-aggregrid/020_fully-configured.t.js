d30a9ddedb2d7b5d02c932596c0e0b3585ae5118
Chris Alfano <chris@jarv.us>
2016-04-15 15:57:35 -0400

    Implement basic refresh event counter

--- sencha-workspace/tests/jarvus-aggregrid/020_fully-configured.t.js
+++ sencha-workspace/tests/jarvus-aggregrid/020_fully-configured.t.js
@@ -3,62 +3,60 @@ StartTest(function(t){
 
     t.requireOk('Jarvus.aggregrid.Aggregrid', function () {
 
-        var AggregridView = Ext.create('Jarvus.aggregrid.Aggregrid', {
-            renderTo: Ext.getBody(),
-            columnsStore: {
-                fields: ['id', 'fullName'],
-                data: [
-                    {id: 1, fullName: 'Ali', rank: 'Pro'},
-                    {id: 2, fullName: 'Chris'},
-                    {id: 3, fullName: 'Ryon'},
-                    {id: 4, fullName: 'Kevin'},
-                    {id: 5, fullName: 'Christian'}
-                ]
-            },
-            columnHeaderTpl: [
-                '{fullName}',
-                '<tpl if="rank">',
-                    ' ({rank})',
-                '</tpl>'
-            ],
+        var refreshCount = 0,
+            AggregridView = Ext.create('Jarvus.aggregrid.Aggregrid', {
+                renderTo: Ext.getBody(),
+                columnsStore: {
+                    fields: ['id', 'fullName'],
+                    data: [
+                        {id: 1, fullName: 'Ali', rank: 'Pro'},
+                        {id: 2, fullName: 'Chris'},
+                        {id: 3, fullName: 'Ryon'},
+                        {id: 4, fullName: 'Kevin'},
+                        {id: 5, fullName: 'Christian'}
+                    ]
+                },
+                columnHeaderTpl: [
+                    '{fullName}',
+                    '<tpl if="rank">',
+                        ' ({rank})',
+                    '</tpl>'
+                ],
 
-            rowsStore: {
-                fields: ['id', 'taskName'],
-                data: [
-                    {id: 1, taskName: 'Task 1'},
-                    {id: 2, taskName: 'Task 2'},
-                    {
-                        id: 3, taskName: 'Task 3',
-                        rows: [
-                            { taskName: 'Task 3 - Subtask 1'},
-                            { taskName: 'Task 3 - Subtask 2'},
-                            { taskName: 'Task 3 - Subtask 3'}
-                        ]
-                    },
-                    {id: 4, taskName: 'Task 4'}
-                ]
-            },
-            rowHeaderField: 'taskName'
+                rowsStore: {
+                    fields: ['id', 'taskName'],
+                    data: [
+                        {id: 1, taskName: 'Task 1'},
+                        {id: 2, taskName: 'Task 2'},
+                        {
+                            id: 3, taskName: 'Task 3',
+                            rows: [
+                                { taskName: 'Task 3 - Subtask 1'},
+                                { taskName: 'Task 3 - Subtask 2'},
+                                { taskName: 'Task 3 - Subtask 3'}
+                            ]
+                        },
+                        {id: 4, taskName: 'Task 4'}
+                    ]
+                },
+                rowHeaderField: 'taskName',
+                listeners: {
+                    refresh: function() {
+                        refreshCount++;
+                    }
+                }
+            });
+
+        t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
+        t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
+
+        // t.waitForComponentVisible(AggregridView, function() {
+        //     // TODO: validate render
+        // });
+
+        t.waitForMs(2000, function() {
+            t.is(refreshCount, 1, 'refresh event fired only once during setup');
+            t.done();
         });
-
-        t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateColumnsStore');
-
-        AggregridView.updateColumnsStore();
-
-        t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after updateRowsStore');
-
-        AggregridView.updateRowsStore();
-
-
-        t.firesOnce(AggregridView, 'refresh', '\'refresh\' event fired once after component instantiation');
-
-
-        t.waitForComponentVisible(AggregridView, function() {
-
-            t.is(AggregridView.columnsStore.getCount(), 5, 'Rendered all 5 students in columnsStore OK');
-            t.is(AggregridView.rowsStore.getCount(), 4, 'Rendered all 4 Tasks in rowsStore OK');
-
-        });
-
     });
 })
