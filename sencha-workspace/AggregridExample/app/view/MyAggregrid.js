750f5fbeefefdfe7786bb5d2e06141750fbacb12
aliyeysides <ali.yeysides@gmail.com>
2016-04-17 16:02:46 -0400

    Add a rowHeadTpl and config stores in view

--- sencha-workspace/AggregridExample/app/view/MyAggregrid.js
+++ sencha-workspace/AggregridExample/app/view/MyAggregrid.js
@@ -2,43 +2,15 @@ Ext.define('AggregridExample.view.MyAggregrid', {
     extend: 'Jarvus.aggregrid.Aggregrid',
     xtype: 'app-myaggregrid',
 
-
     config: {
-        columnsStore: {
-            //type: 'store',
-            fields: ['id', 'fullName'],
-            data: [
-                {id: 1, fullName: 'Ali', rank: 'Pro'},
-                {id: 2, fullName: 'Chris'},
-                {id: 3, fullName: 'Ryon'},
-                {id: 4, fullName: 'Kevin'},
-                {id: 5, fullName: 'Christian'}
-            ]
-        },
+        columnsStore: Ext.create('AggregridExample.store.Student'),
         columnHeaderTpl: [
-            '{fullName}',
-            '<tpl if="rank">',
-                ' ({rank})',
-            '</tpl>'
+            '{fullName}'
         ],
-
-        rowsStore: {
-            //type: 'store',
-            fields: ['id', 'taskName'],
-            data: [
-                {id: 1, taskName: 'Task 1'},
-                {id: 2, taskName: 'Task 2'},
-                {
-                    id: 3, taskName: 'Task 3',
-                    rows: [
-                        { taskName: 'Task 3 - Subtask 1'},
-                        { taskName: 'Task 3 - Subtask 2'},
-                        { taskName: 'Task 3 - Subtask 3'}
-                    ]
-                },
-                {id: 4, taskName: 'Task 4'}
-            ]
-        },
+        rowHeaderTpl: [
+            '{week}'
+        ],
+        rowsStore: Ext.create('AggregridExample.store.TimePeriod'),
         rowHeaderField: 'taskName'
     }
 });
