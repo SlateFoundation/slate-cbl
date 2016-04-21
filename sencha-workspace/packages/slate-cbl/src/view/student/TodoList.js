1fffa0eb7981f99b4ca776118cbe8099ad3114a1
Ryon Coleman <ryon@jarv.us>
2016-04-20 23:37:26 -0400

    Mock up and style student TodoList component

--- sencha-workspace/packages/slate-cbl/src/view/student/TodoList.js
+++ sencha-workspace/packages/slate-cbl/src/view/student/TodoList.js
@@ -1,5 +1,5 @@
 Ext.define('Slate.cbl.view.student.TodoList', {
-    extend: 'Ext.Panel',
+    extend: 'Slate.cbl.widget.SimplePanel',
     xtype: 'slate-todolist',
     requires:[
     ],
@@ -11,11 +11,74 @@ Ext.define('Slate.cbl.view.student.TodoList', {
 
     title: 'To-Do List – Personal',
 
-    data: {
-        baz: 'qux'
-    },
+    data: [
+        {
+            title: 'Active',
+            items: [
+                {
+                    title: 'Apply for scholarship by January 6th: https://scienceleadership.org/scholarship',
+                    category: 'Mythbusters',
+                    date: 'Jan 6, 2016',
+                    status: 'late'
+                },
+                {
+                    title: 'Email Mrs. Johnson about science hw',
+                    category: 'Entrepeneurship',
+                    date: 'May 31, 2016',
+                    status: 'due'
+                },
+                {
+                    title: 'Research internships and apply',
+                    category: 'Narrative Writing Workshop',
+                    date: 'July 1, 2018'
+                }
+            ]
+        },
+        {
+            title: 'Completed',
+            items: [
+                {
+                    title: 'Apply for scholarship by January 6th: https://scienceleadership.org/scholarship',
+                    category: 'Mythbusters',
+                    date: 'Jan 6, 2016',
+                    completed: true
+                },
+                {
+                    title: 'Email Mrs. Johnson about science hw',
+                    category: 'Entrepeneurship',
+                    date: 'May 31, 2016',
+                    completed: true
+                },
+                {
+                    title: 'Research internships and apply',
+                    category: 'Narrative Writing Workshop',
+                    date: 'July 1, 2018',
+                    completed: true
+                }
+            ]
+        }
+    ],
 
     tpl: [
-        '{baz}'
+        '<tpl for=".">',
+            '<section class="slate-todolist-itemgroup">',
+                '<header class="slate-todolist-itemgroup-header">',
+                    '<h4 class="slate-todolist-itemgroup-title">{title} Items</h4>',
+                '</header>',
+                '<ul class="slate-todolist-list">',
+                    '<tpl for="items">',
+                        '<li class="slate-todolist-item <tpl if="status">slate-todolist-status-{status}</tpl>">',
+                            // TODO use better ID?
+                            '<input id="todo-item-{#}" class="slate-todolist-item-checkbox" type="checkbox" <tpl if="completed">checked</tpl>>',
+                            '<div class="slate-todolist-item-text">',
+                                '<label for="todo-item-{#}" class="slate-todolist-item-title">{title}</label>',
+                                '<div class="slate-todolist-item-category">{category}</div>',
+                            '</div>',
+                            '<div class="slate-todolist-item-date">{date:date("M j, Y")}</div>',
+                        '</li>',
+                    '</tpl>',
+                '</ul>',
+            '</section>',
+        '</tpl>'
     ]
 });
