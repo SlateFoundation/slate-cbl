a8939a086d31716f11e2ca6c8d1e17d001570000
Chris Alfano <chris@jarv.us>
2016-04-20 22:00:11 -0400

    Add "Week #" prefix to rowHeaderTpl

--- sencha-workspace/AggregridExample/app/view/MyAggregrid.js
+++ sencha-workspace/AggregridExample/app/view/MyAggregrid.js
@@ -10,7 +10,7 @@ Ext.define('AggregridExample.view.MyAggregrid', {
 
         rowsStore: 'TimePeriods',
         rowHeaderTpl: [
-            '{week}'
+            'Week #{week}'
         ]
     }
 });
