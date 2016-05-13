af3454c28b9d1186691ad98659c7175a7a602eb4
Ryon Coleman <ryon55@gmail.com>
2016-05-13 04:40:04 -0400

    Add task details panel + improvements to tasks manager

--- sencha-workspace/packages/slate-cbl/src/view/teacher/TasksManager.js
+++ sencha-workspace/packages/slate-cbl/src/view/teacher/TasksManager.js
@@ -2,21 +2,28 @@ Ext.define('Slate.cbl.view.teacher.TasksManager', {
     extend: 'Ext.grid.GridPanel',
     xtype: 'slate-tasksmanager',
     requires:[
+        'Slate.cbl.view.teacher.TaskDetails'
     ],
 
     config: {
     },
 
     title: 'Task Database',
+    header: false,
 
     componentCls: 'slate-tasksmanager',
 
-    store: {
-        fields: ['title', 'parent', 'type', 'skills', 'year', 'creator', 'created'],
-        data: [
-            { title: 'Examination of Historical Egypt', parent: 'Mesopotamian Geography',   type: 'Studio', skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'], year: 'Y2', creator: 'Thomas Gaffney',  created: '2015-04-03' }
-        ]
-    },
+    dockedItems: [
+        {
+            xtype: 'pagingtoolbar',
+            dock: 'bottom'
+        },
+        {
+            xtype: 'slate-taskdetails',
+            dock: 'right',
+            width: 240
+        }
+    ],
 
     columns: {
         items: [
@@ -27,6 +34,102 @@ Ext.define('Slate.cbl.view.teacher.TasksManager', {
             { text: 'Year',                 dataIndex: 'year',      width: 64   },
             { text: 'Created by',           dataIndex: 'creator',   width: 160  },
             { text: 'Created',              dataIndex: 'created',   width: 128  }
+        ]
+    },
+
+    store: {
+        fields: ['title', 'parent', 'type', 'skills', 'year', 'creator', 'created'],
+        data: [
+            {
+                title: 'Title of Task',
+                parent: 'Subtask Title',
+                type: 'Workshop',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Chris Alfano',
+                created: '2015-03-12'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Mesopotamian Geography',
+                type: 'Studio',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Thomas Gaffney',
+                created: '2015-04-03'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Subtask Title',
+                type: 'Workshop',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Chris Alfano',
+                created: '2015-03-13'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Mesopotamian Geography',
+                type: 'Studio',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Thomas Gaffney',
+                created: '2015-04-04'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Subtask Title',
+                type: 'Workshop',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Chris Alfano',
+                created: '2015-03-14'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Mesopotamian Geography',
+                type: 'Studio',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Thomas Gaffney',
+                created: '2015-04-06'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Subtask Title',
+                type: 'Mythbusters',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Chris Alfano',
+                created: '2015-03-17'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Mesopotamian Geography',
+                type: 'Studio',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Thomas Gaffney',
+                created: '2015-04-08'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Subtask Title',
+                type: 'Workshop',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Chris Alfano',
+                created: '2015-03-18'
+            },
+            {
+                title: 'Title of Task',
+                parent: 'Mesopotamian Geography',
+                type: 'Studio',
+                skills: ['HIS.1', 'HS.1', 'HIS.2', 'HS.2'],
+                year: 'Y2',
+                creator: 'Thomas Gaffney',
+                created: '2015-04-10'
+            },
         ]
     }
 });
