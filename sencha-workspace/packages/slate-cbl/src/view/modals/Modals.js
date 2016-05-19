c11b47c9ae3279cb973500ff34b24232e15cc43f
Ryon Coleman <ryon55@gmail.com>
2016-05-19 12:03:06 -0400

    Add Rate Task modal

--- sencha-workspace/packages/slate-cbl/src/view/modals/Modals.js
+++ sencha-workspace/packages/slate-cbl/src/view/modals/Modals.js
@@ -1,17 +1,18 @@
 Ext.define('Slate.cbl.view.modals.Modals', {
     extend: 'Ext.Container',
     xtype: 'slate-modals',
-    requires: [ 'Slate.cbl.view.modals.*' ],
 
     layout: {
         type: 'vbox',
         align: 'center'
     },
 
+    margin: '16 16 0',
+
     defaults: {
         xtype: 'button',
         scale: 'large',
-        margin: 16
+        margin: '0 0 16'
     },
 
     items: [
@@ -19,6 +20,12 @@ Ext.define('Slate.cbl.view.modals.Modals', {
             text: 'Create Task',
             handler: function() {
                 Ext.create('Slate.cbl.view.modals.CreateTask').show();
+            }
+        },
+        {
+            text: 'Rate Task',
+            handler: function() {
+                Ext.create('Slate.cbl.view.modals.RateTask').show();
             }
         }
     ]
