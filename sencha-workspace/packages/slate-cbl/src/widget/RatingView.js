35a439a26f3c91afdce23f77cb81c52792d77f72
Ryon Coleman <ryon55@gmail.com>
2016-06-07 04:41:23 -0400

    Polish and colorize RatingView

--- sencha-workspace/packages/slate-cbl/src/widget/RatingView.js
+++ sencha-workspace/packages/slate-cbl/src/widget/RatingView.js
@@ -6,6 +6,7 @@ Ext.define('Slate.cbl.widget.RatingView', {
     componentCls: 'slate-ratingview',
 
     data: {
+        ratings: [ 7, 8, 9, 10, 11, 12, 'M' ],
         competencies: [
             {
                 code: 'ELA.2',
@@ -20,7 +21,7 @@ Ext.define('Slate.cbl.widget.RatingView', {
                     {
                         code: 'ELA.2.HS.3',
                         desc: 'Analyze developments',
-                        level: 9
+                        level: 11
                     }
                 ]
             },
@@ -31,7 +32,8 @@ Ext.define('Slate.cbl.widget.RatingView', {
                     {
                         code: 'ELA.3.HS.2',
                         desc: 'Use evidence to develop claims and counterclaims',
-                        level: 9
+                        level: 9,
+                        rating: 8
                     }
                 ]
             }
@@ -39,26 +41,32 @@ Ext.define('Slate.cbl.widget.RatingView', {
     },
 
     tpl: [
+        '{% this.ratings = values.ratings %}',
+
         '<ul class="slate-ratingview-competencies">',
             '<tpl for="competencies">',
                 '<li class="slate-ratingview-competency">',
                     '<h4 class="slate-ratingview-competency-title">{code}<tpl if="code &amp;&amp; desc"> – </tpl>{desc}</h4>',
                     '<ul class="slate-ratingview-skills">',
                         '<tpl for="skills">',
-                            '<li class="slate-ratingview-skill">',
+                            '<li class="slate-ratingview-skill slate-ratingview-skill-level-{level}">',
                                 '<header class="slate-ratingview-skill-header">',
                                     '<button class="slate-ratingview-remove"><i class="fa fa-times-circle"></i></button>',
                                     '<h5 class="slate-ratingview-skill-title">{code}<tpl if="code &amp;&amp; desc"> – </tpl>{desc}</h5>',
                                 '</header>',
                                 '<ol class="slate-ratingview-ratings">',
-                                    '<li class="slate-ratingview-rating slate-ratingview-rating-null" data-rating="null"><span class="slate-ratingview-rating-label">N/A</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="7"><span class="slate-ratingview-rating-label">7</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="8"><span class="slate-ratingview-rating-label">8</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="9"><span class="slate-ratingview-rating-label">9</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="10"><span class="slate-ratingview-rating-label">10</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="11"><span class="slate-ratingview-rating-label">11</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="12"><span class="slate-ratingview-rating-label">12</span></li>',
-                                    '<li class="slate-ratingview-rating" data-rating="M"><span class="slate-ratingview-rating-label">M</span></li>',
+                                    '<li class="slate-ratingview-rating slate-ratingview-rating-null <tpl if="values.rating == null">is-selected</tpl>" data-rating="null">',
+                                        '<div class="slate-ratingview-rating-bubble" tabindex="0">',
+                                            '<span class="slate-ratingview-rating-label">N/A</span>',
+                                        '</div>',
+                                    '</li>',
+                                    '<tpl for="this.ratings">', // access template-scoped variable declared at top
+                                        '<li class="slate-ratingview-rating <tpl if="values == parent.rating">is-selected</tpl>" data-rating="{.}">',
+                                            '<div class="slate-ratingview-rating-bubble" tabindex="0">',
+                                                '<span class="slate-ratingview-rating-label">{.}</span>',
+                                            '</div>',
+                                        '</li>',
+                                    '</tpl>',
                                 '</ol>',
                             '</li>',
                         '</tpl>',
