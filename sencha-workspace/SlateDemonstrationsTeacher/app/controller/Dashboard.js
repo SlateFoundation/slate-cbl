81b58946423735459f767e5317290730b6ca560f
ali yeysides <ali.yeysides@gmail.com>
2016-06-13 15:21:51 -0400

    Modify Dashboard controller to render Dashboard container

--- sencha-workspace/SlateDemonstrationsTeacher/app/controller/Dashboard.js
+++ sencha-workspace/SlateDemonstrationsTeacher/app/controller/Dashboard.js
@@ -47,10 +47,41 @@ Ext.define('SlateDemonstrationsTeacher.controller.Dashboard', {
     ],
 
     refs: {
-        dashboardCt: 'slate-demonstrations-teacher-dashboard',
-
         studentProgressGrid: 'slate-demonstrations-teacher-dashboard slate-demonstrations-teacher-studentsprogressgrid',
-        teacherOverviewwindow: 'slate-demonstrations-teacher-skill-overviewwindow'
+        teacherOverviewwindow: 'slate-demonstrations-teacher-skill-overviewwindow',
+        dashboardCt: {
+            selector: 'slate-demonstrations-teacher-dashboard',
+            autoCreate: true,
+
+            xtype: 'slate-demonstrations-teacher-dashboard'
+        }
+    },
+
+
+    // controller templates method overrides
+    onLaunch: function () {
+        var siteEnv = window.SiteEnvironment || {},
+            contentAreaCode = (siteEnv.cblContentArea || {}).Code,
+            dashboardCt, progressGrid;
+
+        dashboardCt = this.getDashboardCt();
+        progressGrid = dashboardCt.getProgressGrid();
+
+        // configure dashboard with any available embedded data
+        if (contentAreaCode) {
+            progressGrid.setStudentDashboardLink('/cbl/student-dashboard?content-area=' + encodeURIComponent(contentAreaCode));
+        }
+
+        if (siteEnv.cblStudents) {
+            progressGrid.getStudentsStore().loadData(siteEnv.cblStudents);
+        }
+
+        if (siteEnv.cblCompetencies) {
+            progressGrid.getCompetenciesStore().loadData(siteEnv.cblCompetencies);
+        }
+
+        // render dashboard
+        dashboardCt.render('slateapp-viewport');
     },
 
 
