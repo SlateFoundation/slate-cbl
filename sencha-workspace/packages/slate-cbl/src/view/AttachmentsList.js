cd0d0ffe4b51369de425608b7f20f862f1c7b23e
Ryon Coleman <ryon55@gmail.com>
2016-06-27 03:08:16 -0400

    Add settings menu to AttachmentsList buttons

--- sencha-workspace/packages/slate-cbl/src/view/AttachmentsList.js
+++ sencha-workspace/packages/slate-cbl/src/view/AttachmentsList.js
@@ -11,9 +11,44 @@ Ext.define('Slate.cbl.view.AttachmentsList', {
             '<li class="slate-attachmentslist-item <tpl if="kind">slate-attachment-{kind}</tpl>">',
                 '<span class="slate-attachment-title"><a href="{url}" target=_blank>{title}</a></span>',
                 // TODO hide/show based on whether it's editable
-                '<button class="plain"><i class="fa fa-gear"></i></button>',
-                '<button class="plain"><i class="fa fa-times-circle"></i></button>',
+                '<button class="plain" action="settings"><i class="fa fa-gear"></i></button>',
+                '<button class="plain" action="remove"><i class="fa fa-times-circle"></i></button>',
             '</li>',
         '</tpl>'
-    ]
+    ],
+
+    afterRender: function() {
+        var me = this;
+        me.callParent();
+
+        me.el.on('click', function(ev, t) {
+            var btn = ev.getTarget('button'),
+                action;
+
+            if (btn) {
+                action = btn.getAttribute('action');
+            }
+
+            switch (action) {
+                case 'settings':
+                    Ext.create('Ext.menu.Menu', {
+                        defaults: {
+                            xtype: 'menucheckitem'
+                        },
+                        items: [
+                            {
+                                text: 'View Only',
+                                checked: true
+                            },
+                            {
+                                text: 'Duplicate'
+                            },
+                            {
+                                text: 'Collaborate'
+                            }
+                        ]
+                    }).showAt(Ext.fly(btn).getXY());
+            }
+        }, { delegate: 'button'});
+    }
 });
