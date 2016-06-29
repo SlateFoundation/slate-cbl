a45071f13eb0d5531b9729b1eb942efc41c2a4b5
ali yeysides <ali.yeysides@gmail.com>
2016-06-29 07:45:01 -0400

    Create SlateTasksStudent Dashboard controller

---
+++ sencha-workspace/SlateTasksStudent/app/controller/Dashboard.js
@@ -0,0 +1,64 @@
+/**
+ * TODO:
+ * - move rendering responsibilities to the view?
+ */
+Ext.define('SlateTasksStudent.controller.Dashboard', {
+    extend: 'Ext.app.Controller',
+    requires: [
+        'Slate.API'
+    ],
+
+
+    // controller configuration
+    views: [
+        'TaskTree',
+        'TodoList',
+        'RecentActivity',
+        'Slate.cbl.view.student.OverallProgress',
+        'Slate.cbl.view.student.TaskHistory'
+    ],
+
+    refs: {
+        taskTree: {
+            selector: 'slate-tasktree',
+            autoCreate: true,
+
+            xtype: 'slate-tasktree'
+        },
+        todoList: {
+            selector: 'slate-todolist',
+            autoCreate: true,
+
+            xtype: 'slate-todolist'
+        },
+        recentActivity: {
+            selector: 'slate-recentactivity',
+            autoCreate: true,
+
+            xtype: 'slate-recentactivity'
+        },
+        taskHistory: {
+            selector: 'slate-taskhistory',
+            autoCreate: true,
+
+            xtype: 'slate-taskhistory'
+        },
+        overallProgress: {
+            selector: 'slate-overallprogress',
+            autoCreate: true,
+
+            xtype: 'slate-cbl-student-overallprogress'
+        }
+    },
+
+
+    // controller templates method overrides
+    onLaunch: function () {
+        this.getTaskTree().render('slate-tasktree');
+        this.getTodoList().render('slate-todolist');
+        this.getRecentActivity().render('slate-recentactivity');
+        this.getTaskHistory().render('slate-taskhistory');
+        this.getOverallProgress().render('slate-overallprogress');
+    }
+
+});
