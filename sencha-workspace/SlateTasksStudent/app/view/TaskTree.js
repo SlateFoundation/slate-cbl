b10f7204df5bd04f5c15d86425f77df821032ce1
Bill Clinton <BillClinton@users.noreply.github.com>
2016-07-12 19:05:10 -0400

    Update template with renamed TaskStatus field

--- sencha-workspace/SlateTasksStudent/app/view/TaskTree.js
+++ sencha-workspace/SlateTasksStudent/app/view/TaskTree.js
@@ -97,15 +97,15 @@ Ext.define('SlateTasksStudent.view.TaskTree', {
         '<ul class="slate-tasktree-list">',
 
             '<tpl for="tasks">',
-                '<li class="slate-tasktree-item <tpl if="subtasks">has-subtasks</tpl> slate-tasktree-status-{Status}">',
+                '<li class="slate-tasktree-item <tpl if="subtasks">has-subtasks</tpl> slate-tasktree-status-{TaskStatus}" recordId="{ID}">',
 
                     '<div class="flex-ct">',
                         '<div class="slate-tasktree-nub <tpl if="subtasks">is-clickable</tpl>"></div>', // TODO: ARIA it up
                         '<div class="slate-tasktree-data">',
-                            '<div class="slate-tasktree-category">{Category}</div>',
+                            '<div class="slate-tasktree-category">{Category} - {ID}</div>',
                             '<div class="slate-tasktree-text">',
                                 '<div class="slate-tasktree-title">{Title}</div>',
-                                '<div class="slate-tasktree-status">{[ this.getStatusString(values.Status) ]}</div>',
+                                '<div class="slate-tasktree-status">{[ this.getStatusString(values.TaskStatus) ]}</div>',
                                 '<div class="slate-tasktree-date">{DueDate:date("M d, Y")}</div>',
                             '</div>',
                         '</div>',
@@ -115,14 +115,14 @@ Ext.define('SlateTasksStudent.view.TaskTree', {
                         '<ul class="slate-tasktree-sublist">',
 
                             '<tpl for="subtasks">',
-                                '<li class="slate-tasktree-item <tpl if="subtasks">has-subtasks</tpl> slate-tasktree-status-{Status}">',
+                                '<li class="slate-tasktree-item <tpl if="subtasks">has-subtasks</tpl> slate-tasktree-status-{TaskStatus}">',
 
                                     '<div class="flex-ct">',
                                         '<div class="slate-tasktree-nub"></div>',
                                         '<div class="slate-tasktree-data">',
                                             '<div class="slate-tasktree-text">',
                                                 '<div class="slate-tasktree-title">{Title}</div>',
-                                                '<div class="slate-tasktree-status">{[ this.getStatusString(values.Status) ]}</div>',
+                                                '<div class="slate-tasktree-status">{[ this.getStatusString(values.TaskStatus) ]}</div>',
                                                 '<div class="slate-tasktree-date">{DueDate:date("M d, Y")}</div>',
                                             '</div>',
                                         '</div>',
@@ -156,16 +156,20 @@ Ext.define('SlateTasksStudent.view.TaskTree', {
         scope: 'this',
         click: {
             fn: 'onTreeClick',
-            element: 'el',
-            delegate: '.slate-tasktree-nub.is-clickable'
+            element: 'el'
         }
     },
 
     onTreeClick: function(ev, t) {
-        var target = Ext.get(t);
+        var target = Ext.get(t),
+            parentEl,recordId;
 
         if (target.is('.slate-tasktree-nub.is-clickable')) {
             target.up('.slate-tasktree-item').toggleCls('is-expanded');
+        } else {
+            parentEl = target.up('.slate-tasktree-item');
+            recordId = parentEl.dom.getAttribute('recordId');
+            this.fireEvent('itemclick',recordId);
         }
     }
 });
