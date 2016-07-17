6d9d30d0a4b1b535a9c2e3297099960fd3996bab
Bill Clinton <BillClinton@users.noreply.github.com>
2016-07-17 07:29:39 -0400

    Clean up formatting

--- sencha-workspace/SlateTasksStudent/app/view/TaskDetails.js
+++ sencha-workspace/SlateTasksStudent/app/view/TaskDetails.js
@@ -26,68 +26,60 @@ Ext.define('SlateTasksStudent.view.TaskDetails', {
         }]
     }],
 
-   items: [
+    items: [{
+        xtype: 'slate-modalform',
+        defaultType: 'displayfield',
+        items: [
         {
-            xtype: 'slate-modalform',
-            defaultType: 'displayfield',
-            items: [
-                {
-                    fieldLabel: 'Student',
-                    name: 'FullName'
-                },
-                {
-                    fieldLabel: 'Title',
-                    name: 'Title'
-                },
-                {
-                    fieldLabel: 'Subtask of',
-                    name: 'ParentTask'
-                },
-                {
-                    fieldLabel: 'Type of Experience',
-                    value: 'Workshop'
-                },
-                {
-                    fieldLabel: 'Due Date',
-                    name: 'DueDate',
-                    renderer: Ext.util.Format.dateRenderer('m/d/y')
-                },
-                {
-                    fieldLabel: 'Expiration Date',
-                    name: 'ExpirationDate',
-                    renderer: Ext.util.Format.dateRenderer('m/d/y')
-                },
-                {
-                    xtype: 'fieldcontainer',
-                    fieldLabel: 'Attachments',
-                    items: [
-                        {
-                            xtype: 'slate-attachmentslist',
-                            data: [
-                                {
-                                    kind: 'doc',
-                                    title: 'Document Name'
-                                },
-                                {
-                                    kind: 'folder',
-                                    title: 'Folder Name'
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    xtype: 'slate-ratingview'
-                },
-                {
-                    fieldLabel: 'Submitted Date',
-                    value: '5/9/15'
-                },
-                {
-                    xtype: 'textareafield',
-                    fieldLabel: 'Comments'
-                }
-            ]
-        }
-    ]
+            fieldLabel: 'Student',
+            name: 'FullName'
+        },
+        {
+            fieldLabel: 'Title',
+            name: 'Title'
+        },
+        {
+            fieldLabel: 'Subtask of',
+            name: 'ParentTask'
+        },
+        {
+            fieldLabel: 'Type of Experience',
+            value: 'Workshop'
+        },
+        {
+            fieldLabel: 'Due Date',
+            name: 'DueDate',
+            renderer: Ext.util.Format.dateRenderer('m/d/y')
+        },
+        {
+            fieldLabel: 'Expiration Date',
+            name: 'ExpirationDate',
+            renderer: Ext.util.Format.dateRenderer('m/d/y')
+        },
+        {
+            xtype: 'fieldcontainer',
+            fieldLabel: 'Attachments',
+            items: [{
+                xtype: 'slate-attachmentslist',
+                data: [{
+                    kind: 'doc',
+                    title: 'Document Name'
+                },{
+                    kind: 'folder',
+                    title: 'Folder Name'
+                }]
+            }]
+        },
+        {
+            xtype: 'slate-ratingview'
+        },
+        {
+            fieldLabel: 'Submitted Date',
+            value: '5/9/15'
+        },
+        {
+            xtype: 'textareafield',
+            fieldLabel: 'Comments'
+        }]
+    }]
 });
