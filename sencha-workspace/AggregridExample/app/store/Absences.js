083dc8a53d26a2aef2e5a77456481f952ee897e3
Chris Alfano <chris@jarv.us>
2016-07-21 09:36:30 -0400

    Remove extra variable

--- sencha-workspace/AggregridExample/app/store/Absences.js
+++ sencha-workspace/AggregridExample/app/store/Absences.js
@@ -6,20 +6,19 @@ Ext.define('AggregridExample.store.Absences', {
     },
 
     applyData: function(data) {
-        var randomRecords = [],
-            amount = Math.floor(Math.random() * 500),
+        var amount = Math.floor(Math.random() * 500),
             i = 0;
 
         if (!data) {
-            for (i; i < amount; i++) {
-                randomRecords.push({
+            data = [];
+
+            for (; i < amount; i++) {
+                data.push({
                     id: i + 1,
                     student_id: Math.floor(Math.random() * 20) + 1,
                     date: new Date(2016, Math.floor(Math.random() * 12), Math.floor(Math.random() * 30) + 1, Math.floor(Math.random() * 24), Math.floor(Math.random() * 60))
                 });
             }
-
-            data = randomRecords;
         }
 
         return this.callParent([data]);
