9e9d0a595732fbf05265ee46baef6ff63f9a78d5
Nafis Bey <nafis@jarv.us>
2016-08-16 18:58:27 -0400

    Add combo requirement and remove eslint errors

--- sencha-workspace/SlateTasksTeacher/app/view/AppHeader.js
+++ sencha-workspace/SlateTasksTeacher/app/view/AppHeader.js
@@ -2,7 +2,8 @@ Ext.define('SlateTasksTeacher.view.AppHeader', {
     extend: 'Slate.cbl.view.AppHeader',
     requires: [
         'SlateTasksTeacher.store.CourseSections',
-        'Ext.toolbar.Fill'
+        'Ext.toolbar.Fill',
+        'Ext.form.field.ComboBox'
     ],
     xtype: 'slate-tasks-teacher-appheader',
 
@@ -16,7 +17,7 @@ Ext.define('SlateTasksTeacher.view.AppHeader', {
         cls: 'slate-appheader-title',
         itemId: 'title',
         html: 'Teacher Task Manager'
-    },{
+    }, {
         xtype: 'combo',
         itemId: 'sectionSelect',
         cls: 'slate-course-selector',
@@ -30,21 +31,19 @@ Ext.define('SlateTasksTeacher.view.AppHeader', {
 
         forceSelection: true,
         editable: false
-    },
-    {
+    }, {
         xtype: 'tbfill'
-    },
-    {
+    }, {
         cls: 'primary',
         iconCls: 'x-fa fa-plus',
         action: 'create'
-    },{
+    }, {
         iconCls: 'x-fa fa-pencil',
         action: 'edit',
-        hidden: true //todo: remove?
-    },{
+        hidden: true // todo: remove?
+    }, {
         iconCls: 'x-fa fa-trash-o',
         action: 'delete',
-        hidden: true //todo: remove?
+        hidden: true // todo: remove?
     }]
 });
