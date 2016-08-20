913f15b05120541fb86065593eedf2ca7f4ad9f3
Bill Clinton <BillClinton@users.noreply.github.com>
2016-08-20 06:07:29 -0400

    Add section title to parent tasks

--- sencha-workspace/SlateTasksStudent/app/view/TaskTree.js
+++ sencha-workspace/SlateTasksStudent/app/view/TaskTree.js
@@ -52,7 +52,7 @@ Ext.define('SlateTasksStudent.view.TaskTree', {
                     '<div class="flex-ct">',
                         '<div class="slate-tasktree-nub <tpl if="subtasks">is-clickable</tpl>"></div>', // TODO: ARIA it up
                         '<div class="slate-tasktree-data">',
-                            '<div class="slate-tasktree-category">{Category}</div>',
+                            '<div class="slate-tasktree-category">{SectionTitle}</div>',
                             '<div class="slate-tasktree-text">',
                                 '<div class="slate-tasktree-title">{Title}</div>',
                                 '<div class="slate-tasktree-status">{[ this.getStatusString(values.TaskStatus) ]}</div>',
