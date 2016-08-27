be31c5e416479e6154b70ab3cbccc3684f2be3f7
Chris Alfano <chris@jarv.us>
2016-08-27 20:36:39 +0000

    Add teacher and student CBL links to tools

---
+++ php-config/Slate/UI/Tools.config.d/cbl.php
@@ -0,0 +1,30 @@
+<?php
+
+if (empty($GLOBALS['Session'])) {
+    return;
+}
+
+$cblTools = [
+    '_icon' => 'cbl'
+];
+
+if ($GLOBALS['Session']->hasAccountLevel('Staff')) {
+    $cblTools['Competency Dashboard'] = '/cbl/dashboards/demonstrations/teacher';
+    $cblTools['Teacher Task Dashboard'] = '/cbl/dashboards/tasks/teacher';
+    $cblTools['My Assigned Tasks'] = '/cbl/dashboards/tasks/student';
+    $cblTools['Task Manager'] = '/cbl/dashboards/tasks/manager';
+    
+    $cblTools['Exports'] = [
+        '_icon' => 'export',
+        'Demonstrations' => '/cbl/exports/demonstrations.csv',        
+        'Progress' => [
+            '_href' => '/cbl/exports/progress.csv',
+            '_icon' => 'bar-chart'
+        ]
+    ];
+} elseif ($GLOBALS['Session']->Person && $GLOBALS['Session']->Person->isA(Slate\People\Student::class)) {
+    $cblTools['Competency Dashboard'] = '/cbl/dashboard/demonstrations/student';
+    $cblTools['Task Dashboard'] = '/cbl/dashboards/tasks/student';
+}
+
+Slate\UI\Tools::$tools['Competency-Based Learning'] = $cblTools;
