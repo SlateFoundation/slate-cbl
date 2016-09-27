5090d5fd3eb9e4d2966107494abdb29e52c8547b
Nafis Bey <nafis@jarv.us>
2016-09-27 17:12:13 -0400

    Set student attachments in task rater

--- sencha-workspace/SlateTasksTeacher/app/view/TaskRater.js
+++ sencha-workspace/SlateTasksTeacher/app/view/TaskRater.js
@@ -36,6 +36,7 @@ Ext.define('SlateTasksTeacher.view.TaskRater', {
 
         form.down('[name=StudentFullName]').setValue(studentTask.get('Student').FirstName + ' ' + studentTask.get('Student').LastName);
         form.down('[name=DueDate]').setValue(studentTask.get('DueDate'));
+        form.down('#student-attachments').setAttachments(studentTask.get('Attachments'));
         commentsField.setRecord(studentTask);
         submissionsCmp.setData(studentTask.get('Submissions'));
         skillsField.setSkills(studentTask.get('Skills'), true, false); // appendSkills, editable
