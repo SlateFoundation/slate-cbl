d1a7f13c4adfee45c17f79e036412e9686b9da34
Bill Clinton <BillClinton@users.noreply.github.com>
2016-12-31 13:52:24 -0500

    Create new files for optimized tasks query
    I am creating new files for these changes rather than changing the existing
    files because other branches are using the original files and errors would
    result if these changes were made on the dev server.  Before this branch is
    eventually merged, these changes should be made in the original files both in
    repo and on the server, and these files should be deleted.

---
+++ php-classes/Slate/CBL/Tasks/StudentTasksRequestHandlerNew.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Slate\CBL\Tasks\StudentDashboard;
+
+use Slate\CBL\Tasks\StudentTask;
+use Slate\CBL\Tasks\Task;
+use Slate\Courses\Section;
+use Slate\Courses\SectionsRequestHandler;
+
+class TasksRequestHandler extends \RequestHandler
+{
+    public static $userResponseModes = [
+        'application/json' => 'json'
+    ];
+
+    public static function handleRequest()
+    {
+        $GLOBALS['Session']->requireAuthentication();
+
+        switch ($action = static::shiftPath()) {
+            case '':
+            case false:
+                return static::handleTasksRequest();
+            default:
+                return static::throwNotFoundError();
+        }
+
+    }
+
+    public static function handleTasksRequest()
+    {
+        $student = static::_getRequestedStudent();
+        $courseSection = static::_getRequestedCourseSection();
+
+        $query = '
+            SELECT Tasks.ID as ID,
+                   Tasks.ParentTaskID as ParentID,
+                   Tasks.Title as TaskTitle,
+                   StudentTasks.ID as StudentTaskID,
+                   StudentTasks.TaskStatus as TaskStatus,
+                   StudentTasks.DueDate,
+                   StudentTasks.Submitted as SubmittedTimestamp,
+                   CourseSections.Title as CourseSectionTitle,
+                   CourseSections.Code as CourseSectionCode
+            FROM %1$s StudentTasks
+            JOIN %2$s Tasks on Tasks.ID = StudentTasks.TaskID
+            JOIN %3$s CourseSections on CourseSections.ID = StudentTasks.CourseSectionID
+            WHERE StudentTasks.StudentID = %4$d
+        ';
+
+        if ($courseSection) {
+            $query .= ' AND StudentTasks.CourseSectionID = '.$courseSection->ID;
+        } else {
+            $query .= ' AND StudentTasks.CourseSectionID IS NOT NULL';  // return all
+        }
+
+        try {
+            $tasks = \DB::allRecords($query, [
+                StudentTask::$tableName,    // 1
+                Task::$tableName,           // 2
+                Section::$tableName,        // 3
+                $student->ID                // 4
+            ]);
+        } catch (TableNotFoundException $e) {
+            $tasks = [];
+        }
+
+        return static::respond('tasks',[
+            'success' => true
+            ,'data' => $tasks
+            ,'total' => count($tasks)
+        ]);
+
+    }
+
+    protected static function _getRequestedStudent()
+    {
+        if (
+            !empty($_GET['student']) &&
+            $GLOBALS['Session']->hasAccountLevel('Staff')
+        ) {
+            if (!$Student = PeopleRequestHandler::getRecordByHandle($_GET['student'])) {
+                return static::throwNotFoundError('Student not found');
+            }
+        } else {
+            $Student = $GLOBALS['Session']->Person;
+        }
+
+        return $Student;
+    }
+
+    protected static function _getRequestedCourseSection()
+    {
+        $CourseSection = null;
+
+        if (!empty($_GET['course_section'])) {
+            if (!$CourseSection = SectionsRequestHandler::getRecordByHandle($_GET['course_section'])) {
+                return static::throwNotFoundError('Course Section not found');
+            }
+        }
+
+        return $CourseSection;
+    }
+}
+
