0c8aecd8b4eb87990b7f993cb6e9bc06c0bd740c
Bill Clinton <BillClinton@users.noreply.github.com>
2017-01-04 01:38:36 -0500

    Return newer fields and function to model

--- sencha-workspace/SlateTasksStudent/app/model/StudentTask.js
+++ sencha-workspace/SlateTasksStudent/app/model/StudentTask.js
@@ -11,17 +11,113 @@ Ext.define('SlateTasksStudent.model.StudentTask', {
 
     fields: [
         {
-            name: 'ID',
-    //        type: 'int'
+            name: 'ID'
+//            type: 'int'
         },
         {
             name: 'ParentID',
             type: 'int',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'Created',
+            type: 'date',
+            dateFormat: 'timestamp',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'CreatorID',
+            type: 'int',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'TaskID',
+            reference: 'Task',
+            type: 'int'
+        },
+        {
+            name: 'SectionTitle',
+            type: 'string',
+            mapping: 'Section.Title',
+            persist: false
+        },
+        {
+            name: 'TaskStatus',
+            type: 'string'
+        },
+        {
+            name: 'TaskClass',
+            type: 'string',
+            mapping: 'Task.Class',
+            defaultValue: '\\Slate\\CBL\\Tasks\\ExperienceTask'
+        },
+        {
+            name: 'TaskCreated',
+            type: 'date',
+            mapping: 'Task.Created',
+            dateFormat: 'timestamp',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'Title',
+            type: 'string',
+            mapping: 'Task.Title',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'Status',
+            mapping: 'Task.Status',
+            type: 'string',
+            persist: false
+        },
+        {
+            name: 'DueDate',
+            type: 'date',
+            dateFormat: 'timestamp',
             allowNull: true
         },
         {
+            name: 'ExpirationDate',
+            type: 'date',
+            mapping: 'Task.ExpirationDate',
+            dateFormat: 'timestamp',
+            allowNull: true
+        },
+        {
+            name: 'Submitted',
+            type: 'date',
+            mapping: 'Submitted',
+            dateFormat: 'timestamp',
+            allowNull: true
+        },
+        {
+            name: 'Instructions',
+            type: 'string',
+            mapping: 'Task.Instructions',
+            allowNull: true
+        },
+        {
+            name: 'ParentTaskID',
+            type: 'int',
+            mapping: 'Task.ParentTaskID',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'ParentTaskTitle',
+            type: 'string',
+            persist: false
+        },
+        {
             name: 'StudentTaskID',
-            type: 'int'
+            type: 'int',
+            allowNull: true,
+            persist: false
         },
         {
             name: 'TaskTitle',
@@ -38,34 +134,102 @@ Ext.define('SlateTasksStudent.model.StudentTask', {
             persist: false
         },
         {
-            name: 'Status',
-            mapping: 'TaskStatus',
-            type: 'string'
+            name: 'FirstName',
+            mapping: 'Student.FirstName',
+            type: 'string',
+            persist: false
         },
         {
-            name: 'DueDate',
-            type: 'date',
-            dateFormat: 'c',
-            allowNull: true
+            name: 'LastName',
+            mapping: 'Student.LastName',
+            type: 'string',
+            persist: false
         },
         {
-            name: 'Submitted',
-            type: 'date',
-            mapping: 'SubmittedDate',
-            dateFormat: 'c',
-            allowNull: true
+            name: 'FullName',
+            depends: ['FirstName', 'LastName'],
+            persist: false,
+            convert: function(v, r) {
+                return r.get('FirstName') + ' ' + r.get('LastName');
+            }
+        },
+        {
+            name: 'Competencies',
+            persist: false
+        },
+        {
+            name: 'SkillRatings',
+            type: 'auto',
+            persist: false
         },
         {
             name: 'filtered',
             type: 'boolean',
             persist: false,
             defaultValue: false
+        },
+        {
+            name: 'Comments',
+            persist: false
         }
     ],
 
+    hasMany: [{
+        model: 'Slate.cbl.model.tasks.Attachment',
+        name: 'Attachments',
+        associationKey: 'Attachments'
+    },
+    {
+        model: 'Slate.cbl.model.tasks.Attachment',
+        name: 'TaskAttachments',
+        associationKey: 'Task.Attachments'
+    },
+    {
+        model: 'Slate.cbl.model.Skill',
+        name: 'Skills',
+        associationKey: 'Task.Skills'
+    }],
+
     proxy: {
         type: 'slate-records',
-        url: '/cbl/student-tasks-new'
+        url: '/cbl/student-tasks/assigned',
+        include: [
+            'Submitted',
+            'Student',
+            'Section',
+            'Comments',
+            'Attachments',
+            'Submissions',
+            'TaskSkills',
+            'Task.Attachments',
+            'Task.ParentTask'
+        ]
+    },
+
+
+    getTaskSkillsGroupedByCompetency: function() {
+        var comps = [], compIds = [],
+            skills = this.get('TaskSkills') || [],
+            compIdx, skill,
+            i = 0;
+
+        for (; i < skills.length; i++) {
+            skill = skills[i];
+
+            if ((compIdx = compIds.indexOf(skill.CompetencyCode)) === -1) {
+                compIdx = compIds.length;
+                comps[compIdx] = {
+                    Code: skill.CompetencyCode,
+                    Descriptor: skill.CompetencyDescriptor,
+                    skills: []
+                };
+                compIds.push(skill.CompetencyCode);
+            }
+
+            comps[compIdx].skills.push(skill);
+        }
+
+        return comps;
     }
 
 });
