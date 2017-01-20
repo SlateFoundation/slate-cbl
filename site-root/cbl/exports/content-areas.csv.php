440a2061d00fa782e032b3d97c771226a9d853b1
Nafis Bey <nafis@jarv.us>
2017-01-19 19:14:22 -0500

    Fix `Rated ER` column calculation

--- site-root/cbl/exports/content-areas.csv.php
+++ site-root/cbl/exports/content-areas.csv.php
@@ -177,7 +177,7 @@ foreach ($students as $student) {
                             if ($demonstrationSkill->Override) {
                                 $completedOpportunities = $demonstrationsRequired;
                                 $missedOpportunities = 0;
-                            } elseif ($demonstrationSkill->DemonstratedLevel > 0) {
+                            } elseif ($demonstrationSkill->DemonstratedLevel > 0 && $ratedOpportunities < $demonstrationsRequired) {
                                 $ratedOpportunities++;
                             }
                         }
@@ -186,7 +186,7 @@ foreach ($students as $student) {
                         $totalSkillsWithGrowth += $skillsWithGrowth;
                         $totalCompletedOpportunities += $completedOpportunities;
                         $totalMissedOpportunities += min($missedOpportunities, $demonstrationsRequired - $completedOpportunities);
-                        $totalRatedOpportunities += min($demonstrationsRequired, $ratedOpportunities);
+                        $totalRatedOpportunities += $ratedOpportunities;
                     }
 
                 }
