b63d814dc787216972543ba75b94218e931567b4
Nafis Bey <nafis@jarv.us>
2017-03-17 14:17:39 -0400

    Fix skill evidence requirement import

--- site-root/cbl/import-maps.php
+++ site-root/cbl/import-maps.php
@@ -12,13 +12,13 @@ $mapsCsv = SpreadsheetReader::createFromStream(fopen($mapsCsvUrl, 'r'));
 while ($mapsRow = $mapsCsv->getNextRow()) {
     Debug::dumpVar($mapsRow, false, '$mapRow');
     $mapCsv = SpreadsheetReader::createFromStream(fopen($mapsRow['CSV'], 'r'));
-    
+
     $ContentArea = Slate\CBL\ContentArea::create([
         'Code' => $mapsRow['Code'],
         'Title' => $mapsRow['Title']
     ], true);
     \Debug::dumpVar($ContentArea, false, 'creating content area');
-    
+
     while ($row = $mapCsv->getNextRow()) {
         if ($row['Type'] == 'Competency Statement') {
             $lastCompetency = Slate\CBL\Competency::create([
@@ -29,12 +29,30 @@ while ($mapsRow = $mapsCsv->getNextRow()) {
             ], true);
             \Debug::dumpVar($lastCompetency, false, 'creating competency');
         } elseif ($row['Type'] == 'Standard') {
+            // handle numeric er values
+            if (is_numeric($row['ER'])) {
+                $demonstrationRequirements = [
+                    'default' => $row['ER']
+                ];
+            } else { // handle comma-delimited level:values pairs: i.e 9:2,10:3,default:4
+                $splitERs = explode(',', $row['ER']);
+                foreach ($splitERs as $er) {
+                    list($lvl, $total) = explode(':', $er);
+
+                    if (($lvl != 'default' && !is_numeric($lvl)) || !is_numeric($total)) {
+                        continue;
+                    }
+
+                    $demonstrationRequirements[$lvl] = $total;
+                }
+            }
+
             \Debug::dumpVar(Slate\CBL\Skill::create([
                 'Competency' => $lastCompetency
                 ,'Code' => $row['Code']
                 ,'Descriptor' => $row['Descriptor']
                 ,'Statement' => $row['Statement']
-                ,'DemonstrationsRequired' => $row['ER']
+                ,'DemonstrationsRequired' => $demonstrationRequirements
             ], true), false, 'creating skill');
         }
     }
