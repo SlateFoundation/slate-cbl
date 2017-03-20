da39c6cebba2185983ee11379f1ac0b57d5d1bb8
Nafis Bey <nafis@jarv.us>
2017-03-19 23:36:39 -0400

    Add migration script for skill descriptor column

---
+++ php-migrations/Slate/CBL/20170319_skill-descriptor-field-type.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Slate\CBL;
+
+$newColumnType = 'TEXT';
+
+if (!static::tableExists(Skill::$tableName)) {
+    print('Skipping, table does not exist.');
+    return static::STATUS_SKIPPED;
+}
+
+if (static::getColumnType(Skill::$tableName, 'Descriptor') == $newColumnType) {
+    print('Skipping, column type already migrated.');
+    return static::STATUS_SKIPPED;
+}
+
+\DB::nonQuery("ALTER TABLE " . Skill::$tableName . " CHANGE COLUMN `Descriptor` `Descriptor` $newColumnType NOT NULL");
+
+return static::STATUS_EXECUTED;
