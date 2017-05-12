b4b5b5b3f0cc4ace627a2c38f1110c74ffffaf2b
Nafis Bey <nbey@jarv.us>
2017-05-12 00:02:08 -0400

    Minor code improvements

--- php-migrations/Slate/CBL/20170319_skill-descriptor-field-type.php
+++ php-migrations/Slate/CBL/20170319_skill-descriptor-field-type.php
@@ -2,19 +2,22 @@
 
 namespace Slate\CBL;
 
-$newColumnType = 'TEXT';
+use DB;
 
-if (!static::tableExists(Skill::$tableName)) {
-    print('Skippingo table does not exist.');
+$newColumnType = 'TEXT';
+$skillTable = Skill::$tableName;
+
+if (!static::tableExists($skillTable)) {
+    print('Skipping, table does not exist.');
     return static::STATUS_SKIPPED;
 }
 
-if (static::getColumnType(Skill::$tableName, 'Descriptor') == $newColumnType) {
+if (static::getColumnType($skillTable, 'Descriptor') == $newColumnType) {
     print('Skipping, column type already migrated.');
     return static::STATUS_SKIPPED;
 }
 
-\DB::nonQuery("ALTER TABLE " . Skill::$tableName . " CHANGE COLUMN `Descriptor` `Descriptor` $newColumnType NOT NULL");
-\DB::nonQuery("ALTER TABLE " . Skill::$historyTable . " CHANGE COLUMN `Descriptor` `Descriptor` $newColumnType NOT NULL");
+DB::nonQuery('ALTER TABLE `%s` CHANGE COLUMN `Descriptor` `Descriptor` %s NOT NULL', [$skillTable, $newColumnType]);
+DB::nonQuery('ALTER TABLE `%s` CHANGE COLUMN `Descriptor` `Descriptor` %s NOT NULL', [$skillTable, $newColumnType]);
 
 return static::STATUS_EXECUTED;
