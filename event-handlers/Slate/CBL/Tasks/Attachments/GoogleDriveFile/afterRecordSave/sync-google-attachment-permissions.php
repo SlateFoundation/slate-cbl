d3d14a06d79781d9fd9b8d26dcb08a5e73c8c0c9
Nafis Bey <nbey@jarv.us>
2017-05-18 20:15:20 +0000

    Reconfigure google drive permission syncing logic
    - Trigger google drive file permission pushes via event-handlers

---
+++ event-handlers/Slate/CBL/Tasks/Attachments/GoogleDriveFile/afterRecordSave/sync-google-attachment-permissions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Slate\CBL\Tasks\Attachments;
+
+use Slate\People\Student;
+use Slate\CBL\Tasks\StudentTask;
+
+$Attachment = $_EVENT['Record'];
+
+// student attached a drive file
+if ($Attachment->isNew && $Attachment->ContextClass == StudentTask::class && $Attachment->Creator && $Attachment->Creator->isA(Student::class)) {
+    foreach ($Attachment->Context->Section->Teachers as $Teacher) {
+        $Attachment->syncUserPermissions($Teacher);
+    }
+}
