99dd0921cd8f259e61d8827aa0ab956b0227aeb2
Nafis Bey <nbey@jarv.us>
2017-08-14 01:29:42 -0400

    Allow multiple standard map uploads

--- site-root/cbl/import-maps.php
+++ site-root/cbl/import-maps.php
@@ -2,9 +2,9 @@
 
 $GLOBALS['Session']->requireAccountLevel('Developer');
 
-if (Slate\CBL\ContentArea::getCount() > 0) {
-    die ("Content area database is already populated, aborting");
-}
+use Slate\CBL\ContentArea;
+use Slate\CBL\Competency;
+use Slate\CBL\Skill;
 
 $mapsCsvUrl = 'https://docs.google.com/a/slate.is/spreadsheets/d/1LnjTF5i2CeQrR9EQLB0llomtkpJ_xx38oyjat8wr2_g/export?format=csv&id=1LnjTF5i2CeQrR9EQLB0llomtkpJ_xx38oyjat8wr2_g&gid=1995361518';
 $mapsCsv = SpreadsheetReader::createFromStream(fopen($mapsCsvUrl, 'r'));
@@ -13,52 +13,54 @@ while ($mapsRow = $mapsCsv->getNextRow()) {
     Debug::dumpVar($mapsRow, false, '$mapRow');
     $mapCsv = SpreadsheetReader::createFromStream(fopen($mapsRow['CSV'], 'r'));
 
-    $ContentArea = Slate\CBL\ContentArea::create([
-        'Code' => $mapsRow['Code'],
-        'Title' => $mapsRow['Title']
-    ], true);
-    \Debug::dumpVar($ContentArea, false, 'creating content area');
+    if (!$ContentArea = ContentArea::getByField('Code', $mapsRow['Code'])) {
+        $ContentArea = ContentArea::create([
+            'Code' => $mapsRow['Code'],
+            'Title' => $mapsRow['Title']
+        ], true);
+        Debug::dumpVar($ContentArea, false, 'creating content area');
 
-    while ($row = $mapCsv->getNextRow()) {
-        if ($row['Type'] == 'Competency Statement') {
-            $lastCompetency = Slate\CBL\Competency::create([
-                'ContentArea' => $ContentArea
-                ,'Code' => $row['Code']
-                ,'Descriptor' => $row['Descriptor']
-                ,'Statement' => $row['Statement']
-            ], true);
-            \Debug::dumpVar($lastCompetency, false, 'creating competency');
-        } elseif ($row['Type'] == 'Standard') {
-            // handle numeric er values
-            if (ctype_digit($row['ER'])) {
-                $demonstrationRequirements = [
-                    'default' => $row['ER']
-                ];
-            } else { // handle comma-delimited level:values pairs: i.e 9:2,10:3,default:4
-                $demonstrationRequirements = [];
-                $splitERs = explode(',', $row['ER']);
-                foreach ($splitERs as $er) {
-                    list($lvl, $total) = explode(':', $er);
+        while ($row = $mapCsv->getNextRow()) {
+            if ($row['Type'] == 'Competency Statement') {
+                $lastCompetency = Competency::create([
+                    'ContentArea' => $ContentArea
+                    ,'Code' => $row['Code']
+                    ,'Descriptor' => $row['Descriptor']
+                    ,'Statement' => $row['Statement']
+                ], true);
+                Debug::dumpVar($lastCompetency, false, 'creating competency');
+            } elseif ($row['Type'] == 'Standard') {
+                // handle numeric er values
+                if (ctype_digit($row['ER'])) {
+                    $demonstrationRequirements = [
+                        'default' => $row['ER']
+                    ];
+                } else { // handle comma-delimited level:values pairs: i.e 9:2,10:3,default:4
+                    $demonstrationRequirements = [];
+                    $splitERs = explode(',', $row['ER']);
+                    foreach ($splitERs as $er) {
+                        list($lvl, $total) = explode(':', $er);
 
-                    if (($lvl != 'default' && !ctype_digit($lvl)) || !ctype_digit($total)) {
-                        throw new Exception('Unable to parse evidence requirements for Skill: '.$row['Code']);
+                        if (($lvl != 'default' && !ctype_digit($lvl)) || !ctype_digit($total)) {
+                            throw new Exception('Unable to parse evidence requirements for Skill: '.$row['Code']);
+                        }
+
+                        $demonstrationRequirements[$lvl] = $total;
                     }
 
-                    $demonstrationRequirements[$lvl] = $total;
+                    if (!isset($demonstrationRequirements['default'])) {
+                        $demonstrationRequirements['default'] = 0;
+                    }
                 }
 
-                if (!isset($demonstrationRequirements['default'])) {
-                    $demonstrationRequirements['default'] = 0;
-                }
+                Debug::dumpVar(Skill::create([
+                    'Competency' => $lastCompetency
+                    ,'Code' => $row['Code']
+                    ,'Descriptor' => $row['Descriptor']
+                    ,'Statement' => $row['Statement']
+                    ,'DemonstrationsRequired' => $demonstrationRequirements
+                ], true), false, 'creating skill');
             }
-
-            \Debug::dumpVar(Slate\CBL\Skill::create([
-                'Competency' => $lastCompetency
-                ,'Code' => $row['Code']
-                ,'Descriptor' => $row['Descriptor']
-                ,'Statement' => $row['Statement']
-                ,'DemonstrationsRequired' => $demonstrationRequirements
-            ], true), false, 'creating skill');
         }
     }
 }
