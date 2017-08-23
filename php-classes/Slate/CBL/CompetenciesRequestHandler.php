d76c7accb815fecb9ee7914164110886e5161e01
Nafis Bey <nbey@jarv.us>
2017-08-22 23:46:20 -0400

    Allow users to browse competencies

--- php-classes/Slate/CBL/CompetenciesRequestHandler.php
+++ php-classes/Slate/CBL/CompetenciesRequestHandler.php
@@ -11,6 +11,7 @@ class CompetenciesRequestHandler extends \RecordsRequestHandler
 {
     public static $recordClass = Competency::class;
     public static $browseOrder = 'ContentAreaID, Code';
+    public static $accountLevelBrowse = 'User';
 
     public static function handleBrowseRequest($options = [], $conditions = [], $responseID = null, $responseData = [])
     {
