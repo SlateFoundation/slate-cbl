9e75e1e115f7b0555ff7e7399c298b31ad568cd7
Chris Alfano <chris@jarv.us>
2017-09-02 18:24:59 -0400

    Populate content area status dynamically

--- sencha-workspace/SlateDemonstrationsStudent/app/controller/Dashboard.js
+++ sencha-workspace/SlateDemonstrationsStudent/app/controller/Dashboard.js
@@ -57,6 +57,9 @@ Ext.define('SlateDemonstrationsStudent.controller.Dashboard', {
         contentAreaStatusCmp = this.getContentAreaStatusCmp();
         recentProgressCmp = this.getRecentProgressCmp();
 
+        // configure content area status
+        contentAreaStatusCmp.setContentAreaTitle(cblContentArea.Title);
+
         // configure recent progress component with any available embedded data
         if (cblStudentId) {
             recentProgressCmp.setStudentId(cblStudentId);
@@ -87,7 +90,8 @@ Ext.define('SlateDemonstrationsStudent.controller.Dashboard', {
     // event handlers
     onDashboardCtRender: function(dashboardCt) {
         var studentId = dashboardCt.getStudentId(),
-            competenciesStore = dashboardCt.getCompetenciesStore();
+            competenciesStore = dashboardCt.getCompetenciesStore(),
+            contentAreaStatusCmp = this.getContentAreaStatusCmp();
 
         if (!studentId || !competenciesStore.isLoaded()) { // TODO: check if competencies store is loaded instead
             return;
@@ -97,13 +101,48 @@ Ext.define('SlateDemonstrationsStudent.controller.Dashboard', {
 
         dashboardCt.getCompletionsStore().loadByStudentsAndCompetencies(studentId, competenciesStore.collect('ID'), {
             callback: function(completions) {
-                dashboardCt.add(Ext.Array.map(completions, function(completion) {
-                    return {
+                var minLevel = Infinity,
+                    totalRequired = 0,
+                    totalMissed = 0,
+                    totalComplete = 0,
+                    averageValues = [],
+                    growthValues = [],
+                    cardConfigs = [],
+                    completionsLength = completions.length,
+                    completionIndex = 0,
+                    completion, average, growth;
+
+                for (; completionIndex < completionsLength; completionIndex++) {
+                    completion = completions[completionIndex];
+
+                    minLevel = Math.min(minLevel, completion.get('currentLevel'));
+                    totalRequired += completion.get('demonstrationsRequired');
+                    totalMissed += completion.get('demonstrationsMissed');
+                    totalComplete += completion.get('demonstrationsComplete');
+
+                    if (growth = completion.get('growth')) {
+                        growthValues.push(growth);
+                    }
+
+                    if (average = completion.get('demonstrationsAverage')) {
+                        averageValues.push(average);
+                    }
+
+                    cardConfigs.push({
                         competency: competenciesStore.getById(completion.get('CompetencyID')),
                         completion: completion,
                         autoEl: 'li'
-                    };
-                }));
+                    });
+                }
+
+                contentAreaStatusCmp.setLevel(minLevel);
+                contentAreaStatusCmp.setPercentComplete(100 * totalComplete / totalRequired);
+                contentAreaStatusCmp.setPercentMissed(100 * totalMissed / totalRequired);
+                contentAreaStatusCmp.setMissed(totalMissed);
+                contentAreaStatusCmp.setAverage(Ext.Array.sum(averageValues) / averageValues.length);
+                contentAreaStatusCmp.setGrowth(Ext.Array.sum(growthValues) / growthValues.length);
+
+                dashboardCt.add(cardConfigs);
 
                 dashboardCt.setCompetenciesStatus('loaded');
             }
--- sencha-workspace/SlateDemonstrationsStudent/app/view/ContentAreaStatus.js
+++ sencha-workspace/SlateDemonstrationsStudent/app/view/ContentAreaStatus.js
@@ -5,34 +5,33 @@ Ext.define('SlateDemonstrationsStudent.view.ContentAreaStatus', {
     ],
 
     config: {
-        cls: [
-            'cbl-level-10', // TODO remove, set dynamically instead
-            'cbl-competency-panel',
-            'cbl-contentarea-status'
-        ]
+        // required inputs
+        contentAreaTitle: null,
+        level: null,
+        percentComplete: 0,
+        percentMissed: 0,
+        missed: null,
+        average: null,
+        growth: null,
+
+        // optional config
+        percentFormat: '0%',
+        averageFormat: '0.##',
+        growthFormat: '0.# yr'
     },
 
-    // TODO remove this block of fake data
-    renderData: {
-        percentComplete: 40,
-        level: 10
-    },
-
-    // TODO this is pretty much shared with CompetencyCard... refactor?
+    cls: ['cbl-competency-panel', 'cbl-contentarea-status'],
     renderTpl: [
         '<header class="panel-header">',
-            '<h3 class="header-title">My English Competencies</h3>', // TODO dynamic
+            '<h3 class="header-title">My <span id="{id}-contentAreaTitleEl" data-ref="contentAreaTitleEl">{contentAreaTitle}</span> Competencies</h3>',
         '</header>',
 
         '<div class="panel-body">',
-            '<div id="{id}-meterEl" data-ref="meterEl" class="cbl-progress-meter <tpl if="isAverageLow">is-average-low</tpl>">',
-                '<div id="{id}-meterBarEl" data-ref="meterBarEl" class="cbl-progress-bar" style="width:{percentComplete:defaultValue(0)}%"></div>',
-                // TODO set width dynamically
-                '<div id="{id}-meterBarMissedEl" data-ref="meterBarMissedEl" class="cbl-progress-bar cbl-progress-bar-missed" style="width: 10%; left: {percentComplete:defaultValue(0)}%"></div>',
-                '<div id="{id}-meterLevelEl" data-ref="meterLevelEl" class="cbl-progress-level no-select">Y{[ values.level - 8]}</div>',
-                '<div id="{id}-meterPercentEl" data-ref="meterPercentEl" class="cbl-progress-percent">{percentComplete}%</div>',
-                // moved to stats table below
-                //'<div id="{id}-meterAverageEl" data-ref="meterAverageEl" class="cbl-progress-average" title="Average">{demonstrationsAverage:number(values.$comp.getAverageFormat())}</div>',
+            '<div id="{id}-meterEl" data-ref="meterEl" class="cbl-progress-meter">',
+                '<div id="{id}-meterBarEl" data-ref="meterBarEl" class="cbl-progress-bar" style="width: {percentComplete:number(values.percentFormat)}"></div>',
+                '<div id="{id}-meterBarMissedEl" data-ref="meterBarMissedEl" class="cbl-progress-bar cbl-progress-bar-missed" style="width: {percentMissed:number(values.percentFormat)}; left: {percentComplete:number(values.percentFormat)}"></div>',
+                '<div id="{id}-meterLevelEl" data-ref="meterLevelEl" class="cbl-progress-level no-select"><tpl if="level">Y{level - 8}</tpl></div>',
+                '<div id="{id}-meterPercentEl" data-ref="meterPercentEl" class="cbl-progress-percent">{percentComplete:number(values.percentFormat)}</div>',
             '</div>',
 
             '<div class="stats-ct">',
@@ -41,25 +40,139 @@ Ext.define('SlateDemonstrationsStudent.view.ContentAreaStatus', {
                         '<th>Missed Skills</th>',
                         '<th>Overall Performance Level</th>',
                         '<th>Overall Growth</th>',
+                    '</thead>',
+
                     '<tbody>',
-                        '<td>2</td>',
-                        '<td>{[ this.getPerformanceLevel(values) ]}</td>',
-                        '<td>0.8 yr</td>',
+                        '<td id="{id}-missedEl" data-ref="missedEl">',
+                            '<tpl if="missed !== null">',
+                                '{missed}',
+                            '<tpl else>',
+                                '&mdash;',
+                            '</tpl>',
+                        '</td>',
+                        '<td id="{id}-averageEl" data-ref="averageEl">',
+                            '<tpl if="average">',
+                                '{average:number(values.averageFormat)}',
+                            '<tpl else>',
+                                '&mdash;',
+                            '</tpl>',
+                        '</td>',
+                        '<td id="{id}-growthEl" data-ref="growthEl">',
+                            '<tpl if="growth">',
+                                '{growth:number(values.growthFormat)}',
+                            '<tpl else>',
+                                '&mdash;',
+                            '</tpl>',
+                        '</td>',
                     '</tbody>',
                 '</table>',
             '</div>',
-        '</div>',
-        {
-            getPerformanceLevel: function(values) {
-                var level = false;
+        '</div>'
+    ],
+    childEls: [
+        'contentAreaTitleEl',
+        'meterEl',
+        'meterBarEl',
+        'meterBarMissedEl',
+        'meterLevelEl',
+        'meterPercentEl',
+        'missedEl',
+        'averageEl',
+        'growthEl'
+    ],
 
-                if (values.demonstrationsAverage && values.$comp.getAverageFormat()) {
-                    level = Ext.util.Format.number(values.demonstrationsAverage, values.$comp.getAverageFormat());
-                }
 
-                return level || '&mdash;';
-            }
+    // component template methods
+    initRenderData: function() {
+        var me = this;
+
+        return Ext.apply(this.callParent(), {
+            percentFormat: me.getPercentFormat(),
+            averageFormat: me.getAverageFormat(),
+            growthFormat: me.getGrowthFormat(),
+
+            contentAreaTitle: me.getContentAreaTitle(),
+            level: me.getLevel(),
+            percentComplete: me.getPercentComplete(),
+            percentMissed: me.getPercentMissed(),
+            missed: me.getMissed(),
+            average: me.getAverage(),
+            growth: me.getGrowth()
+        });
+    },
+
+
+    // config handlers
+    updateContentAreaTitle: function(title) {
+        if (this.rendered) {
+            this.contentAreaTitleEl.update(title);
         }
-    ]
+    },
 
+    updateLevel: function(newLevel, oldLevel) {
+        var me = this;
+
+        if (oldLevel) {
+            me.removeCls('cbl-level-' + oldLevel);
+        }
+
+        if (newLevel) {
+            me.addCls('cbl-level-' + newLevel);
+        }
+
+        if (me.rendered) {
+            me.meterLevelEl.update(newLevel ? 'Y'+newLevel : '');
+        }
+    },
+
+    applyPercentComplete: function(percentComplete) {
+        return percentComplete || 0;
+    },
+
+    updatePercentComplete: function(percentComplete) {
+        var me = this;
+
+        if (me.rendered) {
+            percentComplete = Ext.util.Format.number(percentComplete, me.getPercentFormat());
+
+            me.meterBarEl.setStyle('width', percentComplete);
+            me.meterBarMissedEl.setStyle('left', percentComplete);
+            me.meterPercentEl.update(percentComplete);
+        }
+    },
+
+    applyPercentMissed: function(percentMissed) {
+        return percentMissed || 0;
+    },
+
+    updatePercentMissed: function(percentMissed) {
+        var me = this;
+
+        if (me.rendered) {
+            percentMissed = Ext.util.Format.number(percentMissed, me.getPercentFormat());
+            me.meterBarMissedEl.setStyle('width', percentMissed);
+        }
+    },
+
+    updateMissed: function(missed) {
+        if (this.rendered) {
+            this.missedEl.update(Ext.isNumber(missed) ? missed.toString() : '&mdash;');
+        }
+    },
+
+    updateAverage: function(average) {
+        var me = this;
+
+        if (me.rendered) {
+            me.averageEl.update(Ext.util.Format.number(average, me.getAverageFormat()));
+        }
+    },
+
+    updateGrowth: function(growth) {
+        var me = this;
+
+        if (me.rendered) {
+            me.growthEl.update(Ext.util.Format.number(growth, me.getGrowthFormat()));
+        }
+    }
 });
