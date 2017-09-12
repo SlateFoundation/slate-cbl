73e2dc1437c4133f5072875d30ecf3415d3857b1
Nafis Bey <nafis@jarv.us>
2017-09-12 20:06:48 +0000

    Use batch workflow to sync permissions when editing a task
    - Only sync permissions for necessary students / attachments

--- event-handlers/Slate/CBL/Tasks/Task/afterRecordTransaction/sync-google-attachment-permissions.php
+++ event-handlers/Slate/CBL/Tasks/Task/afterRecordTransaction/sync-google-attachment-permissions.php
@@ -7,15 +7,31 @@ use Slate\CBL\Tasks\Attachments\GoogleDriveFile;
 $Task = $_EVENT['Record'];
 $GoogleDriveClass = GoogleDriveFile::class;
 $attachments = [];
+$students = [];
 
 // sync google drive file permssions for when creating new tasks, or editing a task with new attachments
-foreach ($Task->Attachments as $Attachment) {
-    if ($Attachment->isA(GoogleDriveFile::class)) {
-        if ($Attachment->isNew) {
+if ($Task->isNew) {
+    foreach ($Task->Attachments as $Attachment) {
+        if ($Attachment->isA(GoogleDriveFile::class)) {
             $attachments[] = $Attachment;
         }
     }
+} else {
+    foreach ($Task->StudentTasks as $StudentTask) {
+        if ($StudentTask->isNew) {
+            $students[] = $StudentTask->Student;
+        }
+    }
+
+    if (count($students)) {
+        foreach ($Task->Attachments as $Attachment) {
+            if ($Attachment->isA(GoogleDriveFile::class)) {
+                $attachments[] = $Attachment;
+            }
+        }
+    }
 }
+
 if (!empty($attachments)) {
-    GoogleDriveFile::syncUsersPermissions($attachments);
+    GoogleDriveFile::syncUsersPermissions($attachments, $students);
 }
