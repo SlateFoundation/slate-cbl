76161fadb31e27894d582e0a8b5a13296e33d800
Chris Alfano <chris@jarv.us>
2017-10-10 22:14:22 -0400

    Add $minimumRatingOffset instead of subtracting

--- php-classes/Slate/CBL/StudentCompetency.php
+++ php-classes/Slate/CBL/StudentCompetency.php
@@ -340,7 +340,7 @@ class StudentCompetency extends \ActiveRecord
 
         // Require all demonstrations have ratings above minimum
         if (static::$minimumRatingOffset !== null) {
-            $minimumRating = $this->Level - static::$minimumRatingOffset;
+            $minimumRating = $this->Level + static::$minimumRatingOffset;
 
             foreach ($this->getEffectiveDemonstrationsData() as $skillID => $demonstrations) {
                 foreach ($demonstrations as $demonstration) {
