4f74c11b7dceaed4400f744972a29c853845fdf5
Chris Alfano <chris@jarv.us>
2017-10-19 23:32:33 +0000

    Add basic browse/read UI for student competencies

---
+++ php-classes/Slate/CBL/StudentCompetenciesRequestHandler.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Slate\CBL;
+
+use ActiveRecord, RecordsRequestHandler;
+use Emergence\People\PeopleRequestHandler;
+
+
+class StudentCompetenciesRequestHandler extends RecordsRequestHandler
+{
+    public static $recordClass = StudentCompetency::class;
+    public static $accountLevelAPI = 'User';
+    public static $accountLevelBrowse = 'User';
+    public static $accountLevelRead = 'Staff';
+    public static $accountLevelComment = 'Staff';
+    public static $accountLevelWrite = 'Administrator';
+    public static $browseLimitDefault = 100;
+    public static $browseOrder = ['ID' => 'DESC'];
+
+
+    public static function checkReadAccess(ActiveRecord $Record, $suppressLogin = false)
+    {
+        $User = $GLOBALS['Session']->Person;
+
+        if (!$suppressLogin) {
+            $GLOBALS['Session']->requireAuthentication();
+        } elseif (!$User) {
+            return false;
+        }
+
+        if ($User->hasAccountLevel(static::$accountLevelRead)) {
+            return true;
+        }
+
+        return $Record->StudentID === $User->ID;
+    }
+
+    public static function handleBrowseRequest($options = [], $conditions = [], $responseID = null, $responseData = [])
+    {
+        $User = $GLOBALS['Session']->Person;
+
+        if (!$User || !$User->hasAccountLevel('Staff')) {
+            $conditions['StudentID'] = $User->ID;
+        }
+
+        // apply student filter
+        if (!$User) {
+            return static::throwUnauthorizedError('Login required');
+        } elseif (!empty($_GET['student'])) {
+            if (!$Student = PeopleRequestHandler::getRecordByHandle($_GET['student'])) {
+                return static::throwNotFoundError('Student not found');
+            }
+
+            if (!$User || ($Student->ID != $User->ID && !$User->hasAccountLevel('Staff'))) {
+                return static::throwUnauthorizedError('Only staff may browse others\' records');
+            }
+
+            $conditions['StudentID'] = $Student->ID;
+            $responseData['Student'] = $Student;
+        } elseif (!$User->hasAccountLevel('Staff')) {
+            $conditions['StudentID'] = $User->ID;
+            $responseData['Student'] = $User;
+        }
+
+
+        // apply competency filter
+        if (!empty($_GET['competency'])) {
+            if (!$Competency = CompetenciesRequestHandler::getRecordByHandle($_GET['competency'])) {
+                return static::throwNotFoundError('Competency not found');
+            }
+
+            $conditions['CompetencyID'] = $Competency->ID;
+            $responseData['Competency'] = $Competency;
+        }
+
+
+        // apply competency filter
+        if (!empty($_GET['level'])) {
+            if (!ctype_digit($_GET['level'])) {
+                return static::throwInvalidRequestError('Level must be numeric');
+            }
+
+            $conditions['Level'] = $_GET['level'];
+        }
+
+
+        // apply competency filter
+        if (!empty($_GET['entered-via'])) {
+            if (!in_array($_GET['entered-via'], StudentCompetency::getFieldOptions('EnteredVia', 'values'))) {
+                return static::throwInvalidRequestError('Entered Via must be numeric');
+            }
+
+            $conditions['EnteredVia'] = $_GET['entered-via'];
+        }
+
+        return parent::handleBrowseRequest($options, $conditions, $responseID, $responseData);
+    }  
+}
