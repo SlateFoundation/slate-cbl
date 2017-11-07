fef3fbbbcad604cb00ba6085d46345638b3ebf53
Chris Alfano <chris@jarv.us>
2017-11-07 15:13:08 -0500

    Clean indentation

--- sencha-workspace/SlateTasksStudent/app/view/TaskDetails.js
+++ sencha-workspace/SlateTasksStudent/app/view/TaskDetails.js
@@ -35,73 +35,73 @@ Ext.define('SlateTasksStudent.view.TaskDetails', {
         xtype: 'slate-modalform',
         defaultType: 'displayfield',
         items: [
-        {
-            fieldLabel: 'Student',
-            name: 'FullName'
-        },
-        {
-            fieldLabel: 'Title',
-            name: 'Title'
-        },
-        {
-            fieldLabel: 'Instructions',
-            name: 'Instructions'
-        },
-        {
-            fieldLabel: 'Subtask of',
-            name: 'ParentTaskTitle'
-        },
-        {
-            fieldLabel: 'Type of Experience',
-            name: 'ExperienceType'
-        },
-        {
-            fieldLabel: 'Due Date',
-            name: 'DueDate',
-            renderer: Ext.util.Format.dateRenderer('m/d/y')
-        },
-        {
-            fieldLabel: 'Expiration Date',
-            name: 'ExpirationDate',
-            renderer: Ext.util.Format.dateRenderer('m/d/y')
-        },
-        {
-            xtype: 'slate-tasks-attachmentsfield',
-            itemId: 'teacher-attachments',
-            readOnly: true
-        },
-        {
-            fieldLabel: 'Submitted Date',
-            name: 'Submitted',
-            renderer: Ext.util.Format.dateRenderer('m/d/y')
-        },
-        {
-            fieldLabel: 'Submissions',
-            name: 'Submissions',
-            renderer: function(val) {
-                var submissionsLength = val.length,
-                    submissionsText = '',
-                    submissionDate,
-                    i = 0;
+            {
+                fieldLabel: 'Student',
+                name: 'FullName'
+            },
+            {
+                fieldLabel: 'Title',
+                name: 'Title'
+            },
+            {
+                fieldLabel: 'Instructions',
+                name: 'Instructions'
+            },
+            {
+                fieldLabel: 'Subtask of',
+                name: 'ParentTaskTitle'
+            },
+            {
+                fieldLabel: 'Type of Experience',
+                name: 'ExperienceType'
+            },
+            {
+                fieldLabel: 'Due Date',
+                name: 'DueDate',
+                renderer: Ext.util.Format.dateRenderer('m/d/y')
+            },
+            {
+                fieldLabel: 'Expiration Date',
+                name: 'ExpirationDate',
+                renderer: Ext.util.Format.dateRenderer('m/d/y')
+            },
+            {
+                xtype: 'slate-tasks-attachmentsfield',
+                itemId: 'teacher-attachments',
+                readOnly: true
+            },
+            {
+                fieldLabel: 'Submitted Date',
+                name: 'Submitted',
+                renderer: Ext.util.Format.dateRenderer('m/d/y')
+            },
+            {
+                fieldLabel: 'Submissions',
+                name: 'Submissions',
+                renderer: function(val) {
+                    var submissionsLength = val.length,
+                        submissionsText = '',
+                        submissionDate,
+                        i = 0;
 
-                for (; i<submissionsLength; i++) {
-                    submissionDate = Ext.Date.format(new Date(val[i].Created * 1000), 'm/d/y');
-                    submissionsText += '<div>' + submissionDate + '</div>';
+                    for (; i<submissionsLength; i++) {
+                        submissionDate = Ext.Date.format(new Date(val[i].Created * 1000), 'm/d/y');
+                        submissionsText += '<div>' + submissionDate + '</div>';
+                    }
+                    return submissionsText;
                 }
-                return submissionsText;
-            }
-        },
-        {
-            xtype: 'slate-ratingview',
-            readOnly: true
-        },
-        {
-            xtype: 'slate-commentsfield',
-            fieldLabel: 'Teacher Comments'
-        },
-        {
-            xtype: 'slate-tasks-attachmentsfield',
-            itemId: 'student-attachments'
-        }]
+            },
+            {
+                xtype: 'slate-ratingview',
+                readOnly: true
+            },
+            {
+                xtype: 'slate-commentsfield',
+                fieldLabel: 'Teacher Comments'
+            },
+            {
+                xtype: 'slate-tasks-attachmentsfield',
+                itemId: 'student-attachments'
+            }]
     }]
 });
