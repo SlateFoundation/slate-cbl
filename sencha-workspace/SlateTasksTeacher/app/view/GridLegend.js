9f977804faa7d4b74649f7481aa492284a6d6239
Chris Alfano <chris@jarv.us>
2017-12-19 19:26:55 -0500

    Add some docs

--- sencha-workspace/SlateTasksTeacher/app/view/GridLegend.js
+++ sencha-workspace/SlateTasksTeacher/app/view/GridLegend.js
@@ -1,3 +1,7 @@
+/**
+ * Provides a legend for the visual classes used in the students grid to
+ * reflect task status.
+ */
 Ext.define('SlateTasksTeacher.view.GridLegend', {
     extend: 'Ext.Component',
     xtype: 'slate-gridlegend',
@@ -8,7 +12,7 @@ Ext.define('SlateTasksTeacher.view.GridLegend', {
         '    <div class="slate-gridlegend-title">Legend</div>',
         '    <ul class="slate-gridlegend-items">',
         '        <tpl for=".">',
-        // TODO more generic
+        // TODO: more generic
         '            <li class="slate-gridlegend-item slate-task-status-{cls} <tpl for="flags">slate-task-status-{.}</tpl>">',
         '                <span class="slate-gridlegend-swatch"></span>',
         '                <span class="slate-gridlegend-label">{title}</span>',
--- sencha-workspace/packages/slate-cbl/src/widget/Placeholder.js
+++ sencha-workspace/packages/slate-cbl/src/widget/Placeholder.js
@@ -1,3 +1,7 @@
+/**
+ * Simple component providing visual styling for configured html content
+ * used to inform a user why same space on their screen is presently empty.
+ */
 Ext.define('Slate.cbl.widget.Placeholder', {
     extend: 'Ext.Component',
     xtype: 'slate-cbl-placeholder',
