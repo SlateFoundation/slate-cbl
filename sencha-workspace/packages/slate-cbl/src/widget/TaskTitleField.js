3ca8d837e4de0606da824ddac52d9a18ceb371b5
Chris Alfano <chris@jarv.us>
2017-12-19 21:46:50 -0500

    Tune task title field

--- sencha-workspace/packages/slate-cbl/src/widget/TaskTitleField.js
+++ sencha-workspace/packages/slate-cbl/src/widget/TaskTitleField.js
@@ -1,36 +1,44 @@
 Ext.define('Slate.cbl.widget.TaskTitleField', {
     extend: 'Ext.form.field.ComboBox',
+    xtype: 'slate-tasks-titlefield',
     requires: [
         'Slate.cbl.model.tasks.Task'
     ],
 
-    xtype: 'slate-tasks-titlefield',
 
     fieldLabel: 'Title',
     name: 'Title',
     valueField: 'Title',
+    displayField: 'Title',
     queryParam: 'q',
+    minChars: 3,
     autoSelect: false,
+    selectOnTab: false,
     listConfig: {
         cls: 'slate-boundlist'
     },
     store: {
         model: 'Slate.cbl.model.tasks.Task',
-        autoLoad: true
+        proxy: {
+            type: 'slate-cbl-tasks',
+            summary: true,
+            include: ['Creator']
+        },
+        remoteSort: true,
+        sorters: [{
+            property: 'Created',
+            direction: 'DESC'
+        }]
     },
     tpl: [
         '<tpl for=".">',
-            '<li class="x-boundlist-item">',
-                '<div class="slate-boundlist-primary"><span class="slate-boundlist-datum slate-boundlist-title">{Title}</span></div>',
-                '<div class="slate-boundlist-secondary">',
-                    '<span class="slate-boundlist-datum slate-boundlist-creator">{CreatorFullName}</span>',
-                    '<span class="slate-boundlist-datum slate-boundlist-date">{Created:date("M j Y")}</span>',
-                '</div>',
-            '</li>',
+        '    <li class="x-boundlist-item">',
+        '        <div class="slate-boundlist-primary"><span class="slate-boundlist-datum slate-boundlist-title">{Title}</span></div>',
+        '        <div class="slate-boundlist-secondary">',
+        '            <span class="slate-boundlist-datum slate-boundlist-creator">{Creator.FirstName} {Creator.LastName}</span>',
+        '            <span class="slate-boundlist-datum slate-boundlist-date">{Created:date("M j Y")}</span>',
+        '        </div>',
+        '    </li>',
         '</tpl>'
-    ],
-    displayTpl: [
-        '<tpl for=".">{Title}</tpl>'
-    ],
-    selectOnTab: false
+    ]
 });
