2df62618c98aee670518cb26e74aff6b33b8e34f
Chris Alfano <chris@jarv.us>
2017-12-26 01:11:23 -0500

    Update competencies model and setup store with code sorter

--- sencha-workspace/packages/slate-cbl/src/model/Competency.js
+++ sencha-workspace/packages/slate-cbl/src/model/Competency.js
@@ -2,7 +2,10 @@ Ext.define('Slate.cbl.model.Competency', {
     extend: 'Ext.data.Model',
     requires: [
         'Slate.cbl.proxy.Competencies',
-        'Ext.data.identifier.Negative'
+        'Ext.data.identifier.Negative',
+        'Ext.data.validator.Range',
+        'Ext.data.validator.Presence',
+        'Ext.data.validator.Format'
     ],
 
 
@@ -11,29 +14,97 @@ Ext.define('Slate.cbl.model.Competency', {
     identifier: 'negative',
 
     fields: [
-        // server-persisted fields
-        { name: 'ID', type: 'int' },
-        { name: 'ContentAreaID', type: 'int' },
-        { name: 'Code', type: 'string'},
-        { name: 'Descriptor', type: 'string'},
-        { name: 'Statement', type: 'string'},
 
-        // server-provided metadata
-        { name: 'totalDemonstrationsRequired', persist: false},
-        { name: 'minimumAverageOffset', persist: false, type: 'float' }
+        // ActiveRecord fields
+        {
+            name: 'ID',
+            type: 'int',
+            allowNull: true
+        },
+        {
+            name: 'Class',
+            type: 'string',
+            defaultValue: 'Slate\\CBL\\Competency'
+        },
+        {
+            name: 'Created',
+            type: 'date',
+            dateFormat: 'timestamp',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'CreatorID',
+            type: 'int',
+            allowNull: true,
+            persist: false
+        },
+
+        // VersionedRecord fields
+        {
+            name: 'Modified',
+            type: 'date',
+            dateFormat: 'timestamp',
+            allowNull: true,
+            persist: false
+        },
+        {
+            name: 'ModifierID',
+            type: 'int',
+            allowNull: true,
+            persist: false
+        },
+
+        // Competency fields
+        {
+            name: 'ContentAreaID',
+            type: 'int'
+        },
+        {
+            name: 'Code',
+            type: 'string'
+        },
+        {
+            name: 'Descriptor',
+            type: 'string'
+        },
+        {
+            name: 'Statement',
+            type: 'string'
+        }
     ],
 
-    getTotalDemonstrationsRequired: function(userLevel) {
-        var me = this,
-            requirements = me.get('totalDemonstrationsRequired'),
-            total = requirements[userLevel];
+    proxy: 'slate-cbl-competencies',
 
-        if (total !== undefined) {
-            return total;
+    validators: [
+        {
+            field: 'ContentAreaID',
+            type: 'min',
+            min: 1,
+            emptyMessage: 'ContentAreaID is required'
+        },
+        {
+            field: 'Code',
+            type: 'presence',
+            message: 'Code is required'
+        },
+        {
+            field: 'Code',
+            type: 'format',
+            matcher: /^[a-zA-Z][a-zA-Z0-9_:\.-]*$/,
+            message: 'Code can only contain letters, numbers, hyphens, underscores, and periods'
+        },
+        {
+            field: 'Descriptor',
+            type: 'presence',
+            message: 'Descriptor is required'
         }
+    ],
 
-        return requirements['default']; // eslint-disable-dot-notation
-    },
 
-    proxy: 'slate-cbl-competencies'
+    getTotalDemonstrationsRequired: function(level) {
+        var totalDemonstrationsRequired = this.get('totalDemonstrationsRequired');
+
+        return totalDemonstrationsRequired[level] || totalDemonstrationsRequired.default;
+    }
 });
--- sencha-workspace/packages/slate-cbl/src/store/Competencies.js
+++ sencha-workspace/packages/slate-cbl/src/store/Competencies.js
@@ -1,41 +1,24 @@
-/*jslint browser: true, undef: true *//*global Ext,Slate*/
 Ext.define('Slate.cbl.store.Competencies', {
     extend: 'Ext.data.Store',
     requires: [
-        'Slate.cbl.API'
+        /* global Slate */
+        'Slate.sorter.Code'
     ],
 
+
     model: 'Slate.cbl.model.Competency',
-    pageSize: 0,
-
-    constructor: function(config) {
-        console.log('constructing', this.$className, config);
-        config = config || {};
-        config.session = Slate.cbl.API.getSession();
-
-        this.callParent([config]);
-
-        this.loadedContentAreas = {};
+    config: {
+        pageSize: 0,
+        remoteSort: false,
+        sorters: true
     },
-    
-    getAllByContentArea: function(contentArea, callback, scope) {
-        var me = this,
-            loadedContentAreas = this.loadedContentAreas;
 
-        contentArea = contentArea.isModel ? contentArea.getId() : parseInt(contentArea, 10);
 
-        if (contentArea in loadedContentAreas) {
-            return Ext.callback(callback, scope, [loadedContentAreas[contentArea]]);
+    applySorters: function(sorters) {
+        if (sorters === true) {
+            sorters = new Slate.sorter.Code();
         }
 
-        me.load({
-            addRecords: true,
-            params: {
-                'content-area': contentArea
-            },
-            callback: function() {
-                Ext.callback(callback, scope, [loadedContentAreas[contentArea] = me.query('ContentAreaID', contentArea)]);
-            }
-        });
+        return this.callParent([sorters]);
     }
 });
