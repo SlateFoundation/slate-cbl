9b4730df9e299cbec6f1eba7364996a886b064b1
Chris Alfano <chris@jarv.us>
2017-12-27 21:34:00 -0500

    Setup updated data stack for demonstrations and demonstration skills

---
+++ sencha-workspace/packages/slate-cbl/src/store/demonstrations/DemonstrationSkills.js
@@ -0,0 +1,7 @@
+Ext.define('Slate.cbl.store.demonstrations.DemonstrationSkills', {
+    extend: 'Ext.data.Store',
+    alias: 'store.slate-cbl-demonstrationskills',
+
+
+    model: 'Slate.cbl.model.demonstrations.DemonstrationSkill'
+});
