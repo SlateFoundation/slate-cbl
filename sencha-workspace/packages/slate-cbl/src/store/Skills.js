40bbe906bfca3038c9a10a157b54ce8f51b77c7b
Chris Alfano <chris@jarv.us>
2017-12-27 21:10:06 -0500

    Add code sorting to Skills store

--- sencha-workspace/packages/slate-cbl/src/store/Skills.js
+++ sencha-workspace/packages/slate-cbl/src/store/Skills.js
@@ -1,10 +1,25 @@
 Ext.define('Slate.cbl.store.Skills', {
     extend: 'Ext.data.Store',
     alias: 'store.slate-cbl-skills',
+    requires: [
+        /* global Slate */
+        'Slate.sorter.Code'
+    ],
 
 
     model: 'Slate.cbl.model.Skill',
     config: {
-        pageSize: 0
+        pageSize: 0,
+        remoteSort: false,
+        sorters: true
+    },
+
+
+    applySorters: function(sorters) {
+        if (sorters === true) {
+            sorters = new Slate.sorter.Code();
+        }
+
+        return this.callParent([sorters]);
     }
 });
