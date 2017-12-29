d04fe30d7609a6e6a165b0fc4eabc83496bd6042
Chris Alfano <chris@jarv.us>
2017-12-29 04:40:37 +0000

    Include filter objects in /cbl/demonstration-skills responses

--- php-classes/Slate/CBL/Demonstrations/DemonstrationSkillsRequestHandler.php
+++ php-classes/Slate/CBL/Demonstrations/DemonstrationSkillsRequestHandler.php
@@ -10,6 +10,7 @@ use Slate\CBL\SkillsRequestHandler;
 class DemonstrationSkillsRequestHandler extends \RecordsRequestHandler
 {
     public static $recordClass = DemonstrationSkill::class;
+    public static $browseOrder = ['ID' => 'ASC'];
     public static $accountLevelRead = 'Staff';
     public static $accountLevelBrowse = 'User';
     public static $accountLevelComment = 'Staff';
@@ -23,6 +24,7 @@ class DemonstrationSkillsRequestHandler extends \RecordsRequestHandler
             }
 
             $conditions['SkillID'] = $Skill->ID;
+            $responseData['Skill'] = $Skill;
         }
 
         if (!empty($_GET['student'])) {
@@ -45,6 +47,7 @@ class DemonstrationSkillsRequestHandler extends \RecordsRequestHandler
             $demonstrationIds = DB::allValues('ID', 'SELECT ID FROM `%s` WHERE StudentID = %u', [Demonstration::$tableName, $Student->ID]);
 
             $conditions[] = 'DemonstrationID IN ('.(count($demonstrationIds) ? implode(',', $demonstrationIds) : '0').')';
+            $responseData['Student'] = $Student;
         } elseif (!$GLOBALS['Session']->hasAccountLevel('Staff')) {
             return static::throwUnauthorizedError('Only staff may browse all records');
         }
