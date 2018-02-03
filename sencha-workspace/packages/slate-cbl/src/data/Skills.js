9a9c33f7084ffc41285718934aed431f20f7c589
Chris Alfano <chris@jarv.us>
2018-02-03 16:53:39 -0500

    Remove singleton data stores

--- sencha-workspace/packages/slate-cbl/src/data/Skills.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser: true, undef: true *//*global Ext,Slate*/
-Ext.define('Slate.cbl.data.Skills', {
-    extend: 'Slate.cbl.store.Skills',
-    singleton: true,
-	
-    storeId: 'cbl-skills',
-    model: 'Slate.cbl.model.Skill'
-});
