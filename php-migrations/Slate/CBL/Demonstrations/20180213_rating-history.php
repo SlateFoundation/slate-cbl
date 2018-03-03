16cd2828143052e46a036ec08d20341ed0ff975f
Chris Alfano <chris@jarv.us>
2018-03-03 23:25:13 +0000

    Add modified/modifier columns to newly-versioned ratings table

--- php-migrations/Slate/CBL/Demonstrations/20180213_rating-history.php
+++ php-migrations/Slate/CBL/Demonstrations/20180213_rating-history.php
@@ -7,22 +7,39 @@ use DB;
 use SQL;
 
 
+$tableName = DemonstrationSkill::$tableName;
+$historyTableName = DemonstrationSkill::getHistoryTableName();
+$skipped = true;
+
+
 // skip if DemonstrationTask table does not exist or history table already exists
 if (!static::tableExists(DemonstrationSkill::$tableName)) {
     printf("Skipping migration because table `%s` does not yet exist\n", DemonstrationSkill::$tableName);
     return static::STATUS_SKIPPED;
 }
 
-$historyTableName = DemonstrationSkill::getHistoryTableName();
-if (static::tableExists($historyTableName)) {
-    printf("Skipping migration because table `%s` already exists\n", $historyTableName);
-    return static::STATUS_SKIPPED;
+
+// create history table if needed
+if (!static::tableExists($historyTableName)) {
+    print("Creating history table: $historyTableName");
+    DB::multiQuery(SQL::getCreateTable(DemonstrationSkill::class));
+    $skipped = false;
 }
 
 
-print("Creating history table: $historyTableName");
-DB::multiQuery(SQL::getCreateTable(DemonstrationSkill::class));
+// add modified/modifier columns
+if (!static::columnExists($tableName, 'Modified')) {
+    printf("Adding `Modified` column to `%s` table\n", $tableName);
+    DB::nonQuery('ALTER TABLE `%s` ADD `Modified` timestamp NULL default NULL AFTER `CreatorID`', $tableName);
+    $skipped = false;
+}
+
+if (!static::columnExists($tableName, 'ModifierID')) {
+    printf("Adding `ModifierID` column to `%s` table\n", $tableName);
+    DB::nonQuery('ALTER TABLE `%s` ADD `ModifierID` int unsigned NULL default NULL AFTER `Modified`', $tableName);
+    $skipped = false;
+}
 
 
 // finish
-return static::STATUS_EXECUTED;
+return $skipped ? static::STATUS_SKIPPED : static::STATUS_EXECUTED;
