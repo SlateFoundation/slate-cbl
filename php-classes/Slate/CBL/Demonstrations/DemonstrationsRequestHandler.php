0b47e6d36f9943fc83166bf222e2ff03a020f95b
Chris Alfano <chris@jarv.us>
2018-08-20 07:46:44 +0000

    refactor: spin applySkillsData() out of DemonstrationsRequestHandler

--- php-classes/Slate/CBL/Demonstrations/DemonstrationsRequestHandler.php
+++ php-classes/Slate/CBL/Demonstrations/DemonstrationsRequestHandler.php
@@ -3,16 +3,10 @@
 namespace Slate\CBL\Demonstrations;
 
 
-use Exception;
-
 use ActiveRecord;
 use DB;
-use SpreadsheetWriter;
 use TableNotFoundException;
 
-use Slate\CBL\Skill;
-use Slate\CBL\StudentCompetency;
-use Slate\People\Student;
 
 class DemonstrationsRequestHandler extends \RecordsRequestHandler
 {
@@ -59,67 +53,7 @@ class DemonstrationsRequestHandler extends \RecordsRequestHandler
 
         if (isset($demonstrationSkillsData)) {
             $Demonstration->recordAffectedStudentCompetencies();
-
-            // index existing DemonstrationSkill records by SkillID
-            $existingDemonstrationSkills = [];
-
-            foreach ($Demonstration->DemonstrationSkills as $DemonstrationSkill) {
-                $existingDemonstrationSkills[$DemonstrationSkill->SkillID] = $DemonstrationSkill;
-            }
-
-
-            // cache current competency levels so all skills saved in this request target the same level, even if it advances during
-            $competencyLevels = [];
-
-
-            // create new and update existing skills
-            $demonstrationSkills = [];
-            foreach ($demonstrationSkillsData as $demonstrationSkillData) {
-                // skip if DemonstratedLevel and Override is unset or null -- these will be deleted
-                if (!isset($demonstrationSkillData['DemonstratedLevel']) && !isset($demonstrationSkillData['Override'])) {
-                    continue;
-                }
-
-                if (!isset($demonstrationSkillData['SkillID'])) {
-                    throw new Exception('demonstration skill requires SkillID be set');
-                }
-
-                $override = !empty($demonstrationSkillData['Override']);
-                $rating = $override ? null : $demonstrationSkillData['DemonstratedLevel'];
-
-                if ($DemonstrationSkill = $existingDemonstrationSkills[$demonstrationSkillData['SkillID']]) {
-                    if (!empty($demonstrationSkillData['TargetLevel'])) {
-                        $DemonstrationSkill->TargetLevel = $demonstrationSkillData['TargetLevel'];
-                    }
-
-                    $DemonstrationSkill->DemonstratedLevel = $rating;
-                    $DemonstrationSkill->Override = $override;
-                } else {
-                    $DemonstrationSkill = DemonstrationSkill::create([
-                        'SkillID' => $demonstrationSkillData['SkillID'],
-                        'DemonstratedLevel' => $rating,
-                        'Override' => $override
-                    ]);
-
-                    if (!empty($demonstrationSkillData['TargetLevel'])) {
-                        $DemonstrationSkill->TargetLevel = $demonstrationSkillData['TargetLevel'];
-                    } elseif (array_key_exists($DemonstrationSkill->Skill->CompetencyID, $competencyLevels)) {
-                        $DemonstrationSkill->TargetLevel = $competencyLevels[$DemonstrationSkill->Skill->CompetencyID];
-                    } else {
-                        $StudentCompetency = StudentCompetency::getCurrentForStudent($Demonstration->Student, $DemonstrationSkill->Skill->Competency);
-                        $DemonstrationSkill->TargetLevel = $competencyLevels[$DemonstrationSkill->Skill->CompetencyID] = $StudentCompetency ? $StudentCompetency->Level : null;
-                    }
-
-                    // append to existing map to prevent issues if data contains multiple entries for same SkillID
-                    $existingDemonstrationSkills[$demonstrationSkillData['SkillID']] = $DemonstrationSkill;
-                }
-
-                $demonstrationSkills[] = $DemonstrationSkill;
-            }
-
-
-            // write new list to relationship
-            $Demonstration->DemonstrationSkills = $demonstrationSkills;
+            $Demonstration->applySkillsData($demonstrationSkillsData);
         }
     }
 
