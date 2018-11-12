7742a255534dfa9854f23861d5a640a17965bb1d
Chris Alfano <chris@jarv.us>
2018-11-12 23:01:55 +0000

    fix: skip todo indexes migration when table does not exist yet

--- php-migrations/Slate/CBL/Tasks/20181111_todo-indexes.php
+++ php-migrations/Slate/CBL/Tasks/20181111_todo-indexes.php
@@ -4,6 +4,11 @@ namespace Slate\CBL\Tasks;
 
 $status = static::STATUS_SKIPPED;
 
+// skip if table does not exist yet
+if (!static::tableExists(Todo::$tableName)) {
+    printf("Skipping migration because table `%s` does not yet exist\n", Todo::$tableName);
+    return $status;
+}
 
 // ensure StudentID column has index
 if (!static::getColumn(Todo::$tableName, 'StudentID')['COLUMN_KEY']) {
