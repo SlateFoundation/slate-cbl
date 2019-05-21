b529f9556ef4b40dd0e2cf481a1cc253fce59ff2
Nafis Bey <nafis@nafis.me>
2019-05-20 21:50:27 -0400

    fix: Filter out current task in ParentTaskField

--- sencha-workspace/SlateTasksManager/app/store/ParentTasks.js
+++ sencha-workspace/SlateTasksManager/app/store/ParentTasks.js
@@ -2,9 +2,6 @@ Ext.define('SlateTasksManager.store.ParentTasks', {
     extend: 'Ext.data.ChainedStore',
 
     config: {
-        source: 'Tasks',
-        filters: [
-            item => item.data.ParentTaskID === null
-        ]
+        source: 'Tasks'
     },
 });
