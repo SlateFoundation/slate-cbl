2b869b1628ff7fa6f407588cd396519e56a54ef7
Chris Alfano <chris@jarv.us>
2019-05-26 11:18:22 -0700

    feat: add cypress testing environment and basic admin login test

---
+++ cypress/integration/login_admin.js
@@ -0,0 +1,19 @@
+describe('Admin login test', () => {
+    it('Visit the homepage', () => {
+        cy.visit('http://localhost:7080/');
+
+        cy.contains('Log In').click();
+        cy.location('pathname').should('eq', '/login');
+        cy.location('search').should('eq', '?return=%2F');
+
+        cy.focused()
+            .should('have.attr', 'name', '_LOGIN[username]')
+            .type('admin');
+
+        cy.focused().closest('form').find('input[type=password]')
+            .should('have.attr', 'name', '_LOGIN[password]')
+            .type('admin{enter}');
+
+            cy.location('pathname').should('eq', '/');
+        });
+});
