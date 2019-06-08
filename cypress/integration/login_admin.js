a0bc6e12a50d4d33db3d8e7896bf4402372164ca
Nafis Bey <nafis@nafis.me>
2019-06-08 13:40:05 -0400

    test: update admin login tests
    - create logout/toggle modal tests
    - update login test to use modal

--- cypress/integration/login_admin.js
+++ cypress/integration/login_admin.js
@@ -1,11 +1,30 @@
 describe('Admin login test', () => {
-    it('Visit the homepage', () => {
+    it('Should toggle login modal display', () => {
         cy.visit('http://localhost:7080/');
 
+        // should be visible after Log In click
+        cy.get('#login-modal').should('not.be.visible');
         cy.contains('Log In').click();
-        cy.location('pathname').should('eq', '/login');
-        cy.location('search').should('eq', '?return=%2F');
+        cy.get('#login-modal').should('be.visible');
 
+        // Should be hidden after cancel click
+        cy.get('#login-modal').contains('Cancel').click();
+        cy.get('#login-modal').should('not.be.visible');
+
+        // Should be hidden after X click
+        cy.contains('Log In').click();
+        cy.get('#login-modal').should('be.visible');
+        cy.get('.modal-close-button').click();
+        cy.get('#login-modal').should('not.be.visible');
+    });
+
+    it('Should login via Modal', () => {
+        cy.visit('http://localhost:7080/');
+
+        cy.get('#login-modal').should('not.be.visible');
+        cy.contains('Log In').click();
+
+        cy.contains('#login-modal').should('not.have.attr', 'display');
         cy.focused()
             .should('have.attr', 'name', '_LOGIN[username]')
             .type('admin')
@@ -15,6 +34,26 @@ describe('Admin login test', () => {
             .should('have.attr', 'name', '_LOGIN[password]')
             .type('admin{enter}');
 
-            cy.location('pathname').should('eq', '/');
-        });
+        cy.location('pathname').should('eq', '/');
+    });
+
+    it('Should show login error notification', () => {
+        cy.visit('http://localhost:7080/');
+
+        cy.get('#login-modal').should('not.be.visible');
+        cy.contains('Log In').click();
+
+        cy.focused()
+            .should('have.attr', 'name', '_LOGIN[username]')
+            .type('admin')
+            .tab();
+
+        cy.focused()
+            .should('have.attr', 'name', '_LOGIN[password]')
+            .type('incorrectpassword{enter}');
+
+        cy.contains('Sorry!')
+            .parents('div')
+            .should('have.attr', 'class', 'notify error');
+    });
 });
