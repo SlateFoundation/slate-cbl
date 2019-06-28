4cd4711a52ed00fba333fea345495737b477385e
Nafis Bey <nafis@nafis.me>
2019-06-28 13:48:42 -0400

    chore: refactor registration test from `emergence-skeleton` to work in slate

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// from https://github.com/javieraviles/cypress-upload-file-post-form
+Cypress.Commands.add('upload_file', (fileName, fileType = ' ', selector) => {
+    cy.get(selector).then(subject => {
+      cy.fixture(fileName, 'base64')
+        .then(Cypress.Blob.base64StringToBlob)
+        .then(blob => {
+          const el = subject[0]
+          const testFile = new File([blob], fileName, { type: fileType })
+          const dataTransfer = new DataTransfer()
+          dataTransfer.items.add(testFile)
+          el.files = dataTransfer.files
+        })
+    })
+  })
