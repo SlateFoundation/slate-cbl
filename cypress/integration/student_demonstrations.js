1505eaeac0f06b44a2e1eec4eb0d64e37d5dd664
Tyler Wiest <tyler@ontrayapp.com>
2019-10-02 14:55:45 -0400

    refactor: replace login with ajax request in beforeEach()

--- cypress/integration/student_demonstrations.js
+++ cypress/integration/student_demonstrations.js
@@ -10,55 +10,49 @@ describe('Student demonstrations test', () => {
         }
     });
 
+    // authenticate as 'student' user
+    beforeEach(() => {
+        cy.visit('/');
+        cy.request({
+            method: 'POST',
+            url: '/login/?format=json',
+            form: true,
+            body: {
+                '_LOGIN[username]': 'student',
+                '_LOGIN[password]': 'student',
+                '_LOGIN[returnMethod]': 'POST'
+            }
+        });
+    });
+
     it('View single dempnstration rubric as student', () => {
 
-        // Open homepage and login as 'student' via modal
-        cy.visit('/');
-
-        cy.get('#login-modal').should('not.be.visible');
-        cy.contains('Log In').click();
-
-        cy.contains('#login-modal').should('not.have.attr', 'display');
-        cy.focused()
-            .should('have.attr', 'name', '_LOGIN[username]')
-            .type('student')
-            .tab();
-
-        cy.focused()
-            .should('have.attr', 'name', '_LOGIN[password]')
-            .type('student{enter}');
-
-        // Verify login sucessful
-        cy.location('pathname').should('eq', '/');
-        cy.get('.slate-omnibar ul.omnibar-items .omnibar-item:nth-child(5n) a').contains('Student');
-
-        // Open student demonstrations dashboard
+        // open student demonstrations dashboard
         cy.visit('/cbl/dashboards/demonstrations/student');
 
-        // Verify student redirect
+        // verify student redirect
         cy.location('hash').should('eq', '#me');
         cy.get('.slate-appcontainer-bodyWrap .slate-placeholder ')
             .contains('Select a content area to load demonstrations dashboard');
 
         cy.window().then((win) => {
 
-            // Get the selector element
+            // het the selector element
             var selectorEl = win.Ext.ComponentQuery.query('slate-cbl-contentareaselector')[0];
 
-            // Click the selector
+            // click the selector
             cy.get('#' + selectorEl.el.dom.id).click();
 
-            // Get the picker element
+            // get the picker element
             var pickerEl = selectorEl.getPicker();
 
-
-            // Verify and click first element of selector dropdown
+            // verify and click first element of selector dropdown
             cy.get('#' + pickerEl.id + ' li:first-child')
                 .contains('English Language Arts')
                 .click();
 
-            // Verify content loads
+            // verify content loads
             cy.get('.slate-demonstrations-student-competenciessummary span').contains('English Language Arts');
-        })
+        });
     });
 });
