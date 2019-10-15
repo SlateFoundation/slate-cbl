a35817fa4887bb1bfa751292f996c825e098136e
Tyler Wiest <tyler@ontrayapp.com>
2019-10-15 16:59:22 -0400

    feat: add student demonstrations growth metrics test

---
+++ cypress/integration/student_demonstrations_growth.js
@@ -0,0 +1,79 @@
+describe('Student competency growth calculation test', () => {
+
+    // load sample database before tests
+    before(() => {
+        cy.resetDatabase();
+    });
+
+    // authenticate as 'teacher' user
+    beforeEach(() => {
+        cy.loginAs('teacher');
+    });
+
+    it('View sample students progress', () => {
+
+        // open student demonstrations dashboard
+        cy.visit('/cbl/dashboards/demonstrations/student#student/ELA');
+
+        // verify teacher redirect
+        cy.get('.slate-demonstrations-student-competenciessummary .slate-simplepanel-title')
+            .contains('English Language Arts');
+
+        cy.withExt().then(({Ext, extQuerySelector, extQuerySelectorAll}) => {
+
+            // validate section overview metrics
+            var competencySummaryCardId = '.slate-demonstrations-student-competenciessummary';
+
+            cy.get(competencySummaryCardId)
+                .find('div[data-ref="meterPercentEl"]')
+                .contains('74%');
+
+            cy.get(competencySummaryCardId)
+                .find('td[data-ref="missedEl"]')
+                .contains('0');
+
+            cy.get(competencySummaryCardId)
+                .find('td[data-ref="averageEl"]')
+                .contains('10');
+
+            cy.get(competencySummaryCardId)
+                .find('td[data-ref="growthEl"]')
+                .contains('1.3 yr');
+
+            // TODO: fix component query to select card by competency id
+            var studentCompetencyCards = extQuerySelectorAll('slate-demonstrations-student-competencycard');
+
+            // validate the growth calculation for each card
+            studentCompetencyCards.forEach(function(competencyCard) {
+
+                // TODO - add support for additional cards
+                if (competencyCard.competency.id != 6) {
+                    return;
+                }
+
+                var competencyCardId = '#' + competencyCard.id;
+
+                // check baseline rating calculation
+                cy.get(competencyCardId)
+                    .find('span[data-ref="codeEl"]')
+                    .contains('ELA.6');
+
+                cy.get(competencyCardId)
+                    .find('td[data-ref="baselineRatingEl"]')
+                    .contains('9.5');
+
+                cy.get(competencyCardId)
+                    .find('td[data-ref="averageEl"]')
+                    .contains('10');
+
+                cy.get(competencyCardId)
+                    .find('td[data-ref="growthEl"]')
+                    .contains('1.5 yr');
+
+                cy.get(competencyCardId)
+                    .find('div[data-ref="meterPercentEl"]')
+                    .contains('67%');
+            });
+        });
+    });
+});
