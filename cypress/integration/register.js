e2a157619d855641959603dd4cedf87cbe4102c8
Nafis Bey <nafis@nafis.me>
2020-01-17 14:34:40 -0500

    fix: update registration test for disabled registrations

--- cypress/integration/register.js
+++ cypress/integration/register.js
@@ -8,53 +8,8 @@ describe('Registration test', () => {
     it('Register and set up profile', () => {
         cy.visit('/register');
 
-        cy.focused()
-            .should('have.attr', 'name', 'FirstName')
-            .type('Fname')
-            .tab()
-        ;
+        cy.get('.content')
+            .contains('Sorry, self-registration is not currently available. Please contact an administrator.');
 
-        cy.focused()
-            .should('have.attr', 'name', 'LastName')
-            .type('Lname')
-            .tab()
-        ;
-
-        cy.focused()
-            .should('have.attr', 'name', 'Email')
-            .type('email@example.org')
-            .tab()
-        ;
-
-        cy.focused()
-            .should('have.attr', 'name', 'Username')
-            .type('zerocool')
-            .tab()
-        ;
-
-        cy.focused()
-            .should('have.attr', 'name', 'Password')
-            .type('password123')
-            .tab()
-        ;
-
-        cy.focused()
-            .should('have.attr', 'name', 'PasswordConfirm')
-            .type('password1234{enter}')
-        ;
-
-        cy.location('pathname').should('eq', '/register');
-
-        cy.get('.error');
-
-        cy.focused()
-            .should('have.attr', 'name', 'FirstName')
-            .tab()
-            .tab()
-            .tab()
-            .tab()
-            .tab()
-            .type('password123{enter}')
-        ;
     });
 });
