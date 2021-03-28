64ca01523e4e00cb357300ccc8cb21014f97e23d
Chris Alfano <chris@jarv.us>
2021-03-28 23:35:59 +0000

    refactor: refine override migrations

--- php-migrations/Slate/CBL/Demonstrations/20210201-0_demonstration-skill-evidence-weight-column.php
+++ php-migrations/Slate/CBL/Demonstrations/20210201-0_demonstration-skill-evidence-weight-column.php
@@ -2,30 +2,24 @@
 
 namespace Slate\CBL\Demonstrations;
 
-use DB;
 
 $columnName = 'EvidenceWeight';
 $tableName = DemonstrationSkill::$tableName;
 $historyTableName = DemonstrationSkill::getHistoryTableName();
 
+// skip if table doesn't exist yet or already has new column
 if (!static::tableExists($tableName)) {
     printf("Skipping migration because table `%s` does not exist yet\n", $tableName);
     return static::STATUS_SKIPPED;
 }
 
 if (static::columnExists($tableName, $columnName)) {
-    printf("Skipping migration because column `%s`.%s does not exist yet\n", $tableName, $columnName);
+    printf("Skipping migration because column `%s`.%s already exists\n", $tableName, $columnName);
     return static::STATUS_SKIPPED;
 }
 
-printf("Updating table `%s`\n", $tableName);
-DB::nonQuery('ALTER TABLE `%s` ADD `%s` TINYINT(10) NULL DEFAULT NULL', [$tableName, $columnName]);
+// add EvidenceWeight columns
+static::addColumn($tableName, $columnName, 'TINYINT(10) NULL DEFAULT NULL');
+static::addColumn($historyTableName, $columnName, 'TINYINT(10) NULL DEFAULT NULL');
 
-printf("Updating table `%s`\n", $historyTableName);
-DB::nonQuery('ALTER TABLE `%s` ADD `%s` TINYINT(10) NULL DEFAULT NULL', [$historyTableName, $columnName]);
-
-if (static::columnExists($tableName, $columnName)) {
-    return static::STATUS_EXECUTED;
-} else {
-    return static::STATUS_FAILED;
-}
+return static::STATUS_EXECUTED;
