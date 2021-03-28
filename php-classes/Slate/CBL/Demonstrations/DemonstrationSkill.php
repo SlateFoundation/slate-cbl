c66221917f10b4311a3153d27e3e4e4c5cf82be3
Chris Alfano <chris@jarv.us>
2021-03-28 23:36:09 +0000

    fix: change EvidenceWeight default back to 1

--- php-classes/Slate/CBL/Demonstrations/DemonstrationSkill.php
+++ php-classes/Slate/CBL/Demonstrations/DemonstrationSkill.php
@@ -36,7 +36,7 @@ class DemonstrationSkill extends \VersionedRecord
         'EvidenceWeight' => [
             'type' => 'tinyint',
             'unsigned' => true,
-            'default' => null
+            'default' => 1
         ]
     ];
 
