bd57e34d355daa577c7ad824ca58342981165f01
Anderson <laventurepsu1@gmail.com>
2021-08-25 16:22:34 -0400

    style: corrected spacing

--- cypress/integration/student_compentencies.js
+++ cypress/integration/student_compentencies.js
@@ -1,5 +1,4 @@
 describe('Teacher student competencies test', () => {
-
     // load sample database before tests
     before(() => {
         cy.resetDatabase();
@@ -11,38 +10,24 @@ describe('Teacher student competencies test', () => {
     });
 
     it('View student competencies dashboard as teacher', () => {
-
         // open student demonstrations dashboard
         cy.visit('/cbl/dashboards/student-competencies/admin');
 
         // verify teacher redirect
         cy.location('hash').should('eq', '');
 
+        cy.get('.slate-appcontainer-bodyWrap .slate-placeholder').contains(
+            'Select a list of students and a content area to load enrollments dashboard'
+        );
 
-
-        cy.get('.slate-appcontainer-bodyWrap .slate-placeholder')
-            .contains('Select a list of students and a content area to load enrollments dashboard');
-
-
-
-        cy.withExt().then(({Ext, extQuerySelector, extQuerySelectorAll}) => {
-
-
-
-
-
+        cy.withExt().then(({ Ext, extQuerySelector, extQuerySelectorAll }) => {
             // get the 'Rubric' selector element
-            var rubricSelector = extQuerySelector('slate-cbl-contentareaselector');
-
-
-
-
+            var rubricSelector = extQuerySelector(
+                'slate-cbl-contentareaselector'
+            );
 
             // click the selector
             cy.get('#' + rubricSelector.el.dom.id).click();
-
-
-            
 
             // verify and click first element of picker dropdown
             cy.get('#' + rubricSelector.getPicker().id + ' .x-boundlist-item')
@@ -53,7 +38,9 @@ describe('Teacher student competencies test', () => {
             cy.location('hash').should('eq', '#ELA');
 
             // get the 'Students' selector element
-            var studentSelector = extQuerySelector('slate-cbl-studentslistselector');
+            var studentSelector = extQuerySelector(
+                'slate-cbl-studentslistselector'
+            );
 
             // click the selector
             cy.get('#' + studentSelector.el.dom.id)
@@ -70,7 +57,9 @@ describe('Teacher student competencies test', () => {
             cy.location('hash').should('eq', '#ELA/group:example_school');
 
             // verify content loads
-            cy.get('.slate-studentcompetencies-admin-grid').contains('Student Slate');
+            cy.get('.slate-studentcompetencies-admin-grid').contains(
+                'Student Slate'
+            );
         });
     });
 });
