SlateFoundation/slate-cbl

commit 07e28a7452d0d1aa445104637afd8c44fa6d777f

feat: allow for custom labels on exporter forms

html-templates/exports/exports.tpl
@@ -11,8 +11,14 @@
         {/if}
 
         <form method="GET" action="/exports/{$scriptPath|escape}">
-            {foreach key=key item=value from=$script.query}
-                {$label = strtr(ucfirst($key), '_', ' ')}
+            {foreach key=key item=item from=$script.query}
+                {if is_array($item)}
+                  {$label = $item['label']}
+                  {$value = $item['value']}
+                {else}
+                  {$label = strtr(ucfirst($key), '_', ' ')}
+                  {$value = $item}
+                {/if}
 
                 {if $key == 'students'}
                     {field

commit 03364a0eaf9b0de00713ee393babd208289af9da

fix: corrected issue introduced in 07e28a7

data-exporters/slate-cbl/student-competencies.php
@@ -24,14 +24,8 @@ return [
     'readQuery' => function (array $input) {
         $query = [
             'students' => 'all',
-            'content_area' => [
-              'label' => 'Competency Area',
-              'value' => null
-            ],
-            'level' => [
-              'label' => 'Porfolio',
-              'value' => 'all'
-            ]
+            'content_area' => null,
+            'level' => null
         ];
 
         if (!empty($input['students'])) {

html-templates/exports/exports.tpl
@@ -11,14 +11,8 @@
         {/if}
 
         <form method="GET" action="/exports/{$scriptPath|escape}">
-            {foreach key=key item=item from=$script.query}
-                {if is_array($item)}
-                  {$label = $item['label']}
-                  {$value = $item['value']}
-                {else}
-                  {$label = strtr(ucfirst($key), '_', ' ')}
-                  {$value = $item}
-                {/if}
+            {foreach key=key item=value from=$script.query}
+                {$label = strtr(ucfirst($key), '_', ' ')}
 
                 {if $key == 'students'}
                     {field
@@ -60,7 +54,7 @@
                 {elseif $key == 'content_area'}
                     {selectField
                         inputName=$key
-                        label=$label
+                        label='Competency Area'
                         default=$value
                         options=DB::valuesTable('Code', 'Title', 'SELECT Code, Title FROM cbl_content_areas ORDER BY Code')
                         blankOption='any'
@@ -68,7 +62,7 @@
                 {elseif $key == 'level'}
                     {selectField
                         inputName=$key
-                        label=$label
+                        label='Portfolio'
                         default=$value
                         options=DB::allValues('Level', 'SELECT DISTINCT Level FROM cbl_student_competencies ORDER BY Level')
                         useKeyAsValue=no