0c13a9d85a9ca746ba206f541f2de8a89bd5be3e
Anderson <laventurepsu1@gmail.com>
2021-09-06 14:13:54 -0400

    test: specified new selector

--- cypress/integration/login_admin.js
+++ cypress/integration/login_admin.js
@@ -10,7 +10,7 @@ describe('Admin login test', () => {
 
         // should be visible after Log In click
         cy.get('#login-modal').should('not.be.visible');
-        cy.contains('Log In').click();
+        cy.get('.slate-omnibar.mobile-hidden').contains('Log In').click();
         cy.get('#login-modal').should('be.visible');
 
         // Should be hidden after cancel click
@@ -18,7 +18,7 @@ describe('Admin login test', () => {
         cy.get('#login-modal').should('not.be.visible');
 
         // Should be hidden after X click
-        cy.contains('Log In').click();
+        cy.get('.slate-omnibar.mobile-hidden').contains('Log In').click();
         cy.get('#login-modal').should('be.visible');
         cy.get('.modal-close-button').click();
         cy.get('#login-modal').should('not.be.visible');
@@ -28,7 +28,7 @@ describe('Admin login test', () => {
         cy.visit('/');
 
         cy.get('#login-modal').should('not.be.visible');
-        cy.contains('Log In').click();
+        cy.get('.slate-omnibar.mobile-hidden').contains('Log In').click();
 
         cy.focused()
             .should('have.attr', 'name', '_LOGIN[username]')
@@ -48,7 +48,7 @@ describe('Admin login test', () => {
         cy.visit('/');
 
         cy.get('#login-modal').should('not.be.visible');
-        cy.contains('Log In').click();
+        cy.get('.slate-omnibar.mobile-hidden').contains('Log In').click();
 
         cy.contains('#login-modal').should('not.have.attr', 'display');
         cy.focused()
