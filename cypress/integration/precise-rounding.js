710068dba49e814431ab628caa4ead8594b95ff7
Anderson <laventurepsu1@gmail.com>
2021-10-12 12:31:29 -0400

    refactor: moved functions to the bottom of file to improve readability

--- cypress/integration/precise-rounding.js
+++ cypress/integration/precise-rounding.js
@@ -156,153 +156,6 @@ const testCaseFifteen = {
 
 describe('Confirm rounding is consistent across UI, API, and exports', () => {
 
-    // API Tests
-    const checkAPIDataAgainstTestCase = ({student, contentArea, competency, baseline, growth, progress, performanceLevel}) => {
-        cy.loginAs('teacher');
-        cy.server().route('GET', '/cbl/student-competencies*').as('studentCompetencyData');
-        cy.visit(`/cbl/dashboards/demonstrations/student#${student}/${contentArea}`);
-        // ensure that API has loaded required data
-        cy.wait('@studentCompetencyData')
-
-        .should(({ xhr }) => {
-            const data = JSON.parse(xhr.response).data
-            const studentData = data.filter((datum)=> datum.CompetencyID === competency);
-            const expectedBaseLine =  studentData[0].BaselineRating
-            const expectedGrowth = studentData[0].growth
-            const expectedPerformanceLevel = studentData[0].demonstrationsAverage
-
-            cy.wrap(xhr).its('status').should('eq', 200);
-
-            expect(expectedBaseLine,
-                `${contentArea}.${competency} for ${student} API Baseline Value ${expectedBaseLine}: Test Doc Baseline Value ${baseline}`
-            ).to.equal(baseline);
-
-            expect(expectedGrowth,
-                `${contentArea}.${competency} for ${student} API Growth Value ${expectedGrowth}: Test Doc Growth Value ${growth}`
-            ).to.equal(growth);
-
-            // API doesnt have a progress property
-            // expect(    ,
-            //     `${contentArea}.${competency} for ${student} API Completion Percentage Value ${expectedProgress}: Test Doc Completion Percentage Value ${progress}`
-            //     ).to.equal(progress);
-
-            expect(expectedPerformanceLevel,
-                `${contentArea}.${competency} for ${student}  API Performance Level Value ${expectedPerformanceLevel}: Test Doc Perfomance Level Value ${performanceLevel}`
-                ).to.equal(performanceLevel);
-         });
-    };
-
-    // UI Tests
-    const checkUIDataAgainstTestCase = (code, competencyCardId, { baseline, growth, progress, performanceLevel }) => {
-        // check baseline rating calculation
-        cy.get(`#${competencyCardId}`)
-            .find('span[data-ref="codeEl"]')
-            .contains(code);
-
-        if (baseline !== undefined) {
-            cy.get(`#${competencyCardId}`)
-            .find('td[data-ref="baselineRatingEl"]')
-            .contains(baseline === null ? '—' : baseline);
-        };
-
-        if (growth !== undefined) {
-            cy.get(`#${competencyCardId}`)
-            .find('td[data-ref="growthEl"]')
-            .contains(growth === null ? '—' : `${growth} yr`);
-        }
-
-        if (progress !== undefined) {
-            cy.get(`#${competencyCardId}`)
-            .find('div[data-ref="meterPercentEl"]')
-            .contains(progress === null ? '—' : progress);
-        };
-
-        if (performanceLevel !== undefined) {
-            cy.get(`#${competencyCardId}`)
-            .find('td[data-ref="averageEl"]')
-            .contains(performanceLevel === null ? '—' : performanceLevel);
-        };
-    };
-
-  const getAndDisplayUIData = ({student, contentArea, competency, baseline, growth, progress, performanceLevel}) => {
-        cy.loginAs('teacher');
-        cy.server().route('GET', '/cbl/student-competencies*').as('studentCompetencyData');
-        cy.visit(`/cbl/dashboards/demonstrations/student#${student}/${contentArea}`);
-        // ensure that API has loaded required data
-        cy.wait('@studentCompetencyData')
-        .then(() => {
-            cy.wait(500); // wait for dom to render
-            // ensure competency card elements have rendered
-            cy.get('li.slate-demonstrations-student-competencycard')
-                .then(() => {
-                    cy.withExt().then(({extQuerySelector}) => {
-                        const card = extQuerySelector(`slate-demonstrations-student-competencycard{getCompetency().get("Code")=="${contentArea}.${competency}"}`);
-
-                        checkUIDataAgainstTestCase(`${contentArea}.${competency}`, card.id, {
-                            baseline,
-                            growth,
-                            progress,
-                            performanceLevel
-                        });
-                    });
-                });
-        });
-  };
-
-    // CSV Tests
-    const checkCSVDataAgainstTestCase = ({student, contentArea, competency, baseline, growth, progress, performanceLevel} ) => {
-        cy.loginAs('admin');
-        cy.visit('/exports');
-
-        // prepare for form submission that returns back a file
-        // https://on.cypress.io/intercept
-        cy.intercept({ pathname: '/exports/slate-cbl/student-competencies'}, (req) => {
-            req.redirect('/exports')
-        }).as('records');
-
-        cy.get('form[action="/exports/slate-cbl/student-competencies"]').within(() => {
-            cy.get('input[name=students]').type(`{selectall}{backspace}${student}`);
-            cy.get('select[name=content_area]').select(contentArea);
-            cy.get('select[name=level]').select('highest');
-            cy.root().submit();
-        });
-
-        cy.wait('@records').its('request').then((req) => {
-        cy.request(req)
-        .then(({ body, headers }) => {
-            expect(headers).to.have.property('content-type', 'text/csv; charset=utf-8')
-            return csvtojson().fromString(body)
-        }).then((records) => {
-
-            const studentCompetencyRow = records.filter((record)=> {
-                return record.Competency === `${contentArea}.${competency}`
-            });
-
-            const expectedPerformanceLevel = +studentCompetencyRow[0]['Performance Level']
-            const expectedGrowth =  +studentCompetencyRow[0]['Growth']
-            const expectedBaseLine = +studentCompetencyRow[0]['Baseline']
-            const expectedProgress = +studentCompetencyRow[0]['Progress']
-
-            expect(expectedPerformanceLevel,
-            `${contentArea}.${competency} for ${student}  CSV Performance Level Value ${expectedPerformanceLevel}: Test Doc Perfomance Level Value ${performanceLevel}`
-            ).to.equal(performanceLevel);
-
-            expect(expectedGrowth,
-                `${contentArea}.${competency} for ${student} CSV Growth Value ${expectedGrowth}: Test Doc Growth Value ${growth}`
-            ).to.equal(growth);
-
-            expect(expectedBaseLine,
-                `${contentArea}.${competency} for ${student} CSV Baseline Value ${expectedBaseLine}: Test Doc Baseline Value ${baseline}`
-            ).to.equal(baseline);
-
-            expect(expectedProgress,
-                `${contentArea}.${competency} for ${student} CSV Completion Percentage Value ${expectedProgress}: Test Doc Completion Percentage Value ${progress}`
-                ).to.equal(progress/100); // progress is represented as decimal in export
-        });
-        });
-    };
-
-
   it('Student 5 ELA Competency 7', () => {
       checkAPIDataAgainstTestCase(testCaseOne);
       getAndDisplayUIData(testCaseOne);
@@ -393,3 +246,151 @@ describe('Confirm rounding is consistent across UI, API, and exports', () => {
         checkCSVDataAgainstTestCase(testCaseFifteen);
     });
 });
+
+
+
+   // API Tests
+   const checkAPIDataAgainstTestCase = ({student, contentArea, competency, baseline, growth, progress, performanceLevel}) => {
+    cy.loginAs('teacher');
+    cy.server().route('GET', '/cbl/student-competencies*').as('studentCompetencyData');
+    cy.visit(`/cbl/dashboards/demonstrations/student#${student}/${contentArea}`);
+    // ensure that API has loaded required data
+    cy.wait('@studentCompetencyData')
+
+    .should(({ xhr }) => {
+        const data = JSON.parse(xhr.response).data
+        const studentData = data.filter((datum)=> datum.CompetencyID === competency);
+        const expectedBaseLine =  studentData[0].BaselineRating
+        const expectedGrowth = studentData[0].growth
+        const expectedPerformanceLevel = studentData[0].demonstrationsAverage
+
+        cy.wrap(xhr).its('status').should('eq', 200);
+
+        expect(expectedBaseLine,
+            `${contentArea}.${competency} for ${student} API Baseline Value ${expectedBaseLine}: Test Doc Baseline Value ${baseline}`
+        ).to.equal(baseline);
+
+        expect(expectedGrowth,
+            `${contentArea}.${competency} for ${student} API Growth Value ${expectedGrowth}: Test Doc Growth Value ${growth}`
+        ).to.equal(growth);
+
+        // API doesnt have a progress property
+        // expect(    ,
+        //     `${contentArea}.${competency} for ${student} API Completion Percentage Value ${expectedProgress}: Test Doc Completion Percentage Value ${progress}`
+        //     ).to.equal(progress);
+
+        expect(expectedPerformanceLevel,
+            `${contentArea}.${competency} for ${student}  API Performance Level Value ${expectedPerformanceLevel}: Test Doc Perfomance Level Value ${performanceLevel}`
+            ).to.equal(performanceLevel);
+     });
+};
+
+// UI Tests
+const checkUIDataAgainstTestCase = (code, competencyCardId, { baseline, growth, progress, performanceLevel }) => {
+    // check baseline rating calculation
+    cy.get(`#${competencyCardId}`)
+        .find('span[data-ref="codeEl"]')
+        .contains(code);
+
+    if (baseline !== undefined) {
+        cy.get(`#${competencyCardId}`)
+        .find('td[data-ref="baselineRatingEl"]')
+        .contains(baseline === null ? '—' : baseline);
+    };
+
+    if (growth !== undefined) {
+        cy.get(`#${competencyCardId}`)
+        .find('td[data-ref="growthEl"]')
+        .contains(growth === null ? '—' : `${growth} yr`);
+    }
+
+    if (progress !== undefined) {
+        cy.get(`#${competencyCardId}`)
+        .find('div[data-ref="meterPercentEl"]')
+        .contains(progress === null ? '—' : progress);
+    };
+
+    if (performanceLevel !== undefined) {
+        cy.get(`#${competencyCardId}`)
+        .find('td[data-ref="averageEl"]')
+        .contains(performanceLevel === null ? '—' : performanceLevel);
+    };
+};
+
+const getAndDisplayUIData = ({student, contentArea, competency, baseline, growth, progress, performanceLevel}) => {
+    cy.loginAs('teacher');
+    cy.server().route('GET', '/cbl/student-competencies*').as('studentCompetencyData');
+    cy.visit(`/cbl/dashboards/demonstrations/student#${student}/${contentArea}`);
+    // ensure that API has loaded required data
+    cy.wait('@studentCompetencyData')
+    .then(() => {
+        cy.wait(500); // wait for dom to render
+        // ensure competency card elements have rendered
+        cy.get('li.slate-demonstrations-student-competencycard')
+            .then(() => {
+                cy.withExt().then(({extQuerySelector}) => {
+                    const card = extQuerySelector(`slate-demonstrations-student-competencycard{getCompetency().get("Code")=="${contentArea}.${competency}"}`);
+
+                    checkUIDataAgainstTestCase(`${contentArea}.${competency}`, card.id, {
+                        baseline,
+                        growth,
+                        progress,
+                        performanceLevel
+                    });
+                });
+            });
+    });
+};
+
+// CSV Tests
+const checkCSVDataAgainstTestCase = ({student, contentArea, competency, baseline, growth, progress, performanceLevel} ) => {
+    cy.loginAs('admin');
+    cy.visit('/exports');
+
+    // prepare for form submission that returns back a file
+    // https://on.cypress.io/intercept
+    cy.intercept({ pathname: '/exports/slate-cbl/student-competencies'}, (req) => {
+        req.redirect('/exports')
+    }).as('records');
+
+    cy.get('form[action="/exports/slate-cbl/student-competencies"]').within(() => {
+        cy.get('input[name=students]').type(`{selectall}{backspace}${student}`);
+        cy.get('select[name=content_area]').select(contentArea);
+        cy.get('select[name=level]').select('highest');
+        cy.root().submit();
+    });
+
+    cy.wait('@records').its('request').then((req) => {
+    cy.request(req)
+    .then(({ body, headers }) => {
+        expect(headers).to.have.property('content-type', 'text/csv; charset=utf-8')
+        return csvtojson().fromString(body)
+    }).then((records) => {
+
+        const studentCompetencyRow = records.filter((record)=> {
+            return record.Competency === `${contentArea}.${competency}`
+        });
+
+        const expectedPerformanceLevel = +studentCompetencyRow[0]['Performance Level']
+        const expectedGrowth =  +studentCompetencyRow[0]['Growth']
+        const expectedBaseLine = +studentCompetencyRow[0]['Baseline']
+        const expectedProgress = +studentCompetencyRow[0]['Progress']
+
+        expect(expectedPerformanceLevel,
+        `${contentArea}.${competency} for ${student}  CSV Performance Level Value ${expectedPerformanceLevel}: Test Doc Perfomance Level Value ${performanceLevel}`
+        ).to.equal(performanceLevel);
+
+        expect(expectedGrowth,
+            `${contentArea}.${competency} for ${student} CSV Growth Value ${expectedGrowth}: Test Doc Growth Value ${growth}`
+        ).to.equal(growth);
+
+        expect(expectedBaseLine,
+            `${contentArea}.${competency} for ${student} CSV Baseline Value ${expectedBaseLine}: Test Doc Baseline Value ${baseline}`
+        ).to.equal(baseline);
+
+        expect(expectedProgress,
+            `${contentArea}.${competency} for ${student} CSV Completion Percentage Value ${expectedProgress}: Test Doc Completion Percentage Value ${progress}`
+            ).to.equal(progress/100); // progress is represented as decimal in export
+    });
+    });
+};
