d2bea6abb6ad50f3504c35dad6df7adf3e645127
BillClinton <bclinton@appunto.net>
2022-03-25 01:18:15 -0400

    feat: implement disabled student filter.
    This is a work in progress.  This file lives in the Slate repo and I am overridding it
    here, but I am still unsure if this is a permanent solution, if the changes should be
    moved to the Slate repo, or if I should create a custom section particiants request handler
    for Slate-cbl.

---
+++ php-classes/Slate/Courses/SectionParticipantsRequestHandler.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Slate\Courses;
+
+
+class SectionParticipantsRequestHandler extends \Slate\RecordsRequestHandler
+{
+    public static $recordClass = SectionParticipant::class;
+
+    public static $accountLevelBrowse = 'Staff';
+    public static $accountLevelWrite = 'Staff';
+    public static $accountLevelRead = 'Staff';
+    public static $accountLevelAPI = 'Staff';
+
+
+    protected static function buildBrowseConditions(array $conditions = [], array &$filterObjects = [])
+    {
+        $conditions = parent::buildBrowseConditions($conditions, $filterObjects);
+
+        if ($Section = static::getRequestedSection()) {
+            $conditions['CourseSectionID'] = $Section->ID;
+            $filterObjects['Section'] = $Section;
+        } elseif ($Term = static::getRequestedTerm()) {
+            $courseSectionIds = array_map(function($section) {
+                return $section->ID;
+            }, Section::getAllByWhere([
+                'TermID' => [
+                    'operator' => 'IN',
+                    'values' => $Term->getContainedTermIDs()
+                ]
+            ]));
+
+            $conditions['CourseSectionID'] = [
+                'operator' => 'IN',
+                'values' => $courseSectionIds
+            ];
+        }
+
+        if ($Student = static::getRequestedStudent()) {
+            $conditions['PersonID'] = $Student->ID;
+            $filterObjects['Student'] = $Student;
+        }
+
+        if (!empty($_REQUEST['cohort'])) {
+            $conditions['Cohort'] = $_REQUEST['cohort'];
+        }
+
+        if (!empty($_REQUEST['role'])) {
+            $conditions['Role'] = [
+                'values' => is_string($_REQUEST['role']) ? explode(',', $_REQUEST['role']) : $_REQUEST['role']
+            ];
+        }
+
+        // apply disabled students filter
+        if (!static::getRequestedStudentDisabledFilter()) {
+          $disabledStudentIds = \DB::allValues(
+              'ID',
+              '
+                  SELECT ID
+                    FROM `%s`
+                  WHERE AccountLevel = "disabled"
+              ',
+              [ \Emergence\People\Person::$tableName ]
+          );
+
+          if (!empty($disabledStudentIds)) {
+              $excludeDisabled = [
+                  'operator' => 'NOT IN',
+                  'values' => $disabledStudentIds
+              ];
+              $existingPersonIDCondition = $conditions['PersonID'];
+
+              if (isset($existingStudentIDCondition)) {
+                  $conditions['PersonID'] = [$existingPersonIDCondition, $excludeDisabled];
+              } else {
+                  $conditions['PersonID'] = $excludeDisabled;
+              }
+          }
+      }
+
+        return $conditions;
+    }
+
+    public static function getRequestedStudentDisabledFilter($fieldName = 'include_deactivated_students')
+    {
+        if (empty($_REQUEST[$fieldName])) {
+            return null;
+        }
+
+        if ($_REQUEST[$fieldName] === 'false') {
+            $_REQUEST[$fieldName] = false;
+        }
+
+        return !!$_REQUEST[$fieldName];
+    }
+}
