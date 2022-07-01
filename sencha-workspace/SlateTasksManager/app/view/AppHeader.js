fb60d06ed8fd6a0d1a6ee984921619c1d38b4c25
BillClinton <bclinton@appunto.net>
2022-07-01 03:51:35 -0400

    feat: restore app header after moving checkbox

--- sencha-workspace/SlateTasksManager/app/view/AppHeader.js
+++ sencha-workspace/SlateTasksManager/app/view/AppHeader.js
@@ -4,44 +4,27 @@ Ext.define('SlateTasksManager.view.AppHeader', {
     xtype: 'slate-tasks-manager-appheader',
     padding: '16 90 16 90',
 
-    layout: 'hbox',
-
     items: [{
         xtype: 'component',
         cls: 'slate-apptitle',
         itemId: 'title',
-        html: 'Task Library',
-        flex: 1
+        html: 'Task Library'
+    },
+        '->',
+    {
+        cls: 'primary',
+        iconCls: 'x-fa fa-plus',
+        action: 'create',
+        tooltip: 'Create Task'
     },{
-        xtype: 'checkbox',
-        name : 'include-archived',
-        value: false,
-        boxLabel: 'include archived',
-        flex: 1
+        iconCls: 'x-fa fa-pencil',
+        action: 'edit',
+        tooltip: 'Edit Task',
+        disabled: true
     },{
-        xtype: 'container',
-        flex: 1,
-        align: 'right',
-        defaults: {
-            xtype: 'button',
-            margin: '0 20px 0 0',
-            cls: 'primary'
-        },
-        items: [{
-            iconCls: 'x-fa fa-plus',
-            action: 'create',
-            tooltip: 'Create Task'
-        },{
-            iconCls: 'x-fa fa-pencil',
-            action: 'edit',
-            tooltip: 'Edit Task',
-            disabled: true
-        },{
-            iconCls: 'x-fa fa-trash-o',
-            action: 'delete',
-            tooltip: 'Delete Task',
-            disabled: true
-        }]
+        iconCls: 'x-fa fa-trash-o',
+        action: 'delete',
+        tooltip: 'Delete Task',
+        disabled: true
     }]
-
 });
