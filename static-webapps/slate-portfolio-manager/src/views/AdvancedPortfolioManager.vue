b090b84c293b83eb462c74920676c50b21a8b5d1
chriscauley <379151+chriscauley@users.noreply.github.com>
2022-09-21 09:14:39 -0400

    removing dummy requests from main view

--- static-webapps/slate-portfolio-manager/src/views/AdvancedPortfolioManager.vue
+++ static-webapps/slate-portfolio-manager/src/views/AdvancedPortfolioManager.vue
@@ -46,11 +46,6 @@ import AdvancedPortfolioSidebar from '@/components/AdvancedPortfolioSidebar.vue'
 import CompetencyDropdown from '@/components/CompetencyDropdown.vue';
 import EnrollmentsGrid from '@/components/EnrollmentsGrid.vue';
 import useAuth from '@/store/useAuth';
-import useContentArea from '@/store/useContentArea';
-import useStudentList from '@/store/useStudentList';
-
-const _log = (...args) => console.log(...args) // eslint-disable-line
-const _table = (...args) => console.table(...args) // eslint-disable-line
 
 export default {
   name: 'AdvancedPortfolioManager',
@@ -67,7 +62,7 @@ export default {
   },
 
   computed: {
-    ...mapStores(useAuth, useContentArea, useStudentList),
+    ...mapStores(useAuth),
     visible: {
       get() {
         return !!this.selected;
@@ -82,112 +77,6 @@ export default {
 
   async mounted() {
     await this.authStore.required();
-    const { user, token } = this.authStore;
-    _log(`user has session, token=${token}, welcome ${user.FirstName}!`);
-
-    // common fetch options
-    const fetchOptions = {
-      credentials: 'include',
-      headers: {
-        Accept: 'application/json',
-      },
-    };
-
-    // get competencies and students lists for navigation selectors
-    const [
-      competencyAreasResponse,
-      studentListsResponse,
-    ] = await Promise.all([
-      fetch('http://localhost:2190/cbl/content-areas?summary=true', fetchOptions).then((response) => response.json()),
-      fetch('http://localhost:2190/people/*student-lists', fetchOptions).then((response) => response.json()),
-    ]);
-    _log('competencyAreasResponse=%o', competencyAreasResponse);
-    _table(competencyAreasResponse.data);
-    _log('studentListsResponse=%o', studentListsResponse);
-    _table(studentListsResponse.data);
-
-    const selectedCompetencyArea = 'ELA';
-    const selectedStudentsList = 'group:class_of_2021';
-
-    // load all intersecting StudentCompetency records, with no embedded data
-    const studentCompetenciesResponse = await fetch(
-      'http://localhost:2190/cbl/student-competencies'
-      + '?limit=0'
-      + '&summary=true'
-      + `&students=${selectedStudentsList}`
-      + `&content_area=${selectedCompetencyArea}`,
-      fetchOptions,
-    ).then((response) => response.json());
-
-    _log('studentCompetenciesResponse=%o', studentCompetenciesResponse);
-    _table(studentCompetenciesResponse.data);
-
-    // fake selection
-    const selectedCompetency = 'ELA.1';
-    const selectedStudent = 'ccross';
-
-    _log('selectedCompetency=%o', selectedCompetency);
-    _log('selectedStudent=%o', selectedStudent);
-
-    // load all details needed to render sidebar
-    const studentCompetencyDetailsResponse = await fetch(
-      'http://localhost:2190/cbl/student-competencies'
-      + '?limit=0'
-      + `&student=${selectedStudent}`
-      + `&competency=${selectedCompetency}`
-      + '&include[]=demonstrationsAverage' // top bar
-      + '&include[]=growth' // top bar
-      + '&include[]=progress' // top bar
-      + '&include[]=Skills'
-      + '&include[]=effectiveDemonstrationsData'
-      + '&include[]=ineffectiveDemonstrationsData',
-      fetchOptions,
-    ).then((response) => response.json());
-
-    _log('studentCompetencyDetailsResponse=%o', studentCompetencyDetailsResponse);
-
-    // build a de-duplicated list of referenced demonstrations to fetch metadata details
-    /**
-     * NOTE: typically there are ways to get this just embedded, but this is a complex case
-     * because "DemonstrationsData" are composite data products subject to some policy/optimization
-     * and not directly 1:1 with records that could be serialized
-     * */
-    const demonstrationIds = new Set();
-    studentCompetencyDetailsResponse.data.forEach((studentCompetency) => {
-      Object.values(studentCompetency.effectiveDemonstrationsData)
-        .forEach((skillDemonstrations) => {
-          skillDemonstrations.forEach((demonstrationData) => {
-            demonstrationIds.add(demonstrationData.DemonstrationID);
-          });
-        });
-      Object.values(studentCompetency.ineffectiveDemonstrationsData)
-        .forEach((skillDemonstrations) => {
-          skillDemonstrations.forEach((demonstrationData) => {
-            demonstrationIds.add(demonstrationData.DemonstrationID);
-          });
-        });
-    });
-
-    // fetch details of all referenced demonstrations
-    const demonstrationDetailsResponse = await fetch(
-      'http://localhost:2190/cbl/demonstrations'
-      + `?q=id:${Array.from(demonstrationIds.values()).join(',')}`
-      + '&include[]=Creator'
-      + '&include[]=StudentTask.Task',
-      fetchOptions,
-    ).then((response) => response.json());
-
-    _log('demonstrationDetailsResponse=%o', demonstrationDetailsResponse);
-    _table(demonstrationDetailsResponse.data.map((demonstration) => ({
-      DemonstrationID: demonstration.ID,
-      Created: demonstration.Created,
-      Creator: `${demonstration.Creator.FirstName} ${demonstration.Creator.LastName}`,
-      DemonstrationDate: (new Date(demonstration.Demonstrated * 1000)).toLocaleDateString(),
-      HasTaskLink: Boolean(demonstration.StudentTask),
-      Description: demonstration.StudentTask
-        ? demonstration.StudentTask.Task.Title
-        : demonstration.PerformanceType,
-    })));
   },
 
   methods: {
