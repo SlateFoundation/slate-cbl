8735356fe64aa17f2b4a953bc159c549a163374c
Chris Alfano <chris@jarv.us>
2022-12-27 17:39:48 -0500

    test(e2e/api/ratings): add test for adding rating

--- cypress/integration/cbl/api/ratings.js
+++ cypress/integration/cbl/api/ratings.js
@@ -488,7 +488,7 @@ describe('CBL / API / Ratings', () => {
         });
     });
 
-    it('demonstrations: Delete a rating', () => {
+    it('demonstrations: Delete a single rating', () => {
         cy.request('POST', '/cbl/demonstrations/save?format=json&include=DemonstrationSkills', {
             data: [{
                 ID: 28,
@@ -564,6 +564,98 @@ describe('CBL / API / Ratings', () => {
             });
 
             expect(response.body.data).property('effectiveDemonstrationsData').to.be.an('object').to.have.all.keys('2', '3', '4');
+            expect(response.body.data.effectiveDemonstrationsData['2']).to.be.an('array').that.has.length(1);
+            expect(response.body.data.effectiveDemonstrationsData['3']).to.be.an('array').that.has.length(1);
+            expect(response.body.data.effectiveDemonstrationsData['4']).to.be.an('array').that.has.length(1);
+        });
+    });
+
+    it('demonstrations: Add a single rating', () => {
+        cy.request('POST', '/cbl/demonstrations/save?format=json&include=DemonstrationSkills', {
+            data: [{
+                ID: 28,
+                DemonstrationSkills: [
+                    { SkillID: 1, TargetLevel: 10, DemonstratedLevel: 12 },
+                    { SkillID: 2, TargetLevel: 10 },
+                    { SkillID: 3, TargetLevel: 10 },
+                    { SkillID: 4, TargetLevel: 10 },
+                ]
+            }]
+        }).then(response => {
+            expect(response).property('status').to.eq(200);
+            expect(response).property('body').to.be.an('object');
+            expect(response.body).property('data').to.be.an('array').that.has.length(1);
+            expect(response.body).property('success', true);
+            expect(response.body).property('failed').to.be.an('array').that.has.length(0);
+            expect(response.body.data[0]).to.be.an('object').to.include({
+                ID: 28,
+                Class: 'Slate\\CBL\\Demonstrations\\ExperienceDemonstration',
+                StudentID: 4,
+            });
+            expect(response.body.data[0].DemonstrationSkills).to.be.an('array').that.has.length(4);
+            expect(response.body.data[0].DemonstrationSkills[0]).to.be.an('object').to.include({
+                Class: 'Slate\\CBL\\Demonstrations\\DemonstrationSkill',
+                DemonstrationID: response.body.data[0].ID,
+                SkillID: 1,
+                TargetLevel: 10,
+                DemonstratedLevel: 12,
+                EvidenceWeight: 1,
+            });
+            expect(response.body.data[0].DemonstrationSkills[1]).to.be.an('object').to.include({
+                Class: 'Slate\\CBL\\Demonstrations\\DemonstrationSkill',
+                DemonstrationID: response.body.data[0].ID,
+                SkillID: 2,
+                TargetLevel: 10,
+                DemonstratedLevel: 9,
+                EvidenceWeight: 1,
+            });
+            expect(response.body.data[0].DemonstrationSkills[2]).to.be.an('object').to.include({
+                Class: 'Slate\\CBL\\Demonstrations\\DemonstrationSkill',
+                DemonstrationID: response.body.data[0].ID,
+                SkillID: 3,
+                TargetLevel: 10,
+                DemonstratedLevel: 10,
+                EvidenceWeight: 1,
+            });
+            expect(response.body.data[0].DemonstrationSkills[3]).to.be.an('object').to.include({
+                Class: 'Slate\\CBL\\Demonstrations\\DemonstrationSkill',
+                DemonstrationID: response.body.data[0].ID,
+                SkillID: 4,
+                TargetLevel: 10,
+                DemonstratedLevel: 10,
+                EvidenceWeight: 1,
+            });
+        });
+
+        cy.request('/cbl/student-competencies/33?format=json&include=completion,effectiveDemonstrationsData').then(response => {
+            expect(response).property('status').to.eq(200);
+            expect(response).property('body').to.be.an('object');
+
+            expect(response.body).property('data').to.be.an('object');
+            expect(response.body.data).to.include({
+                ID: 33,
+                StudentID: 4,
+                CompetencyID: 1,
+                Level: 10,
+                EnteredVia: 'graduation',
+                BaselineRating: 10
+            });
+
+            expect(response.body.data).property('completion').to.be.an('object');
+            expect(response.body.data.completion).to.include({
+                StudentID: 4,
+                CompetencyID: 1,
+                currentLevel: 10,
+                baselineRating: 10,
+                demonstrationsLogged: 4,
+                demonstrationsMissed: 0,
+                demonstrationsComplete: 4,
+                demonstrationsAverage: 10.3,
+                demonstrationsRequired: 12
+            });
+
+            expect(response.body.data).property('effectiveDemonstrationsData').to.be.an('object').to.have.all.keys('1', '2', '3', '4');
+            expect(response.body.data.effectiveDemonstrationsData['1']).to.be.an('array').that.has.length(1);
             expect(response.body.data.effectiveDemonstrationsData['2']).to.be.an('array').that.has.length(1);
             expect(response.body.data.effectiveDemonstrationsData['3']).to.be.an('array').that.has.length(1);
             expect(response.body.data.effectiveDemonstrationsData['4']).to.be.an('array').that.has.length(1);
