a0ed991f549a1d970d89cd709ffcccac23dec3e1
Ryon Coleman <ryon55@gmail.com>
2023-01-01 21:55:46 -0500

    feat: add first-pass draft of ratings Rater to replace Slider

---
+++ sencha-workspace/packages/slate-cbl/src/field/ratings/Rater.js
@@ -0,0 +1,96 @@
+Ext.define('Slate.cbl.field.ratings.Rater', {
+    extend: 'Ext.Container',
+    xtype: 'slate-cbl-ratings-rater',
+    requires: [
+        'Slate.cbl.model.Skill'
+    ],
+
+    config: {
+        skill: null,
+        level: null,
+    },
+
+    componentCls: 'slate-cbl-ratings-rater',
+
+    items: [{
+        xtype: 'label',
+        cls: 'x-form-item-label',
+        text: 'Skill code and descriptor',
+    }, {
+        xtype: 'container',
+        layout: 'hbox',
+        margin: '4 0',
+        items: [{
+            xtype: 'button',
+            scale: 'large',
+            text: 'Clear',
+            cls: 'slate-cbl-ratings-rater-clear-btn',
+        }, {
+            flex: 1,
+            xtype: 'segmentedbutton',
+            margin: '0 0 0 16',
+            defaults: {
+                scale: 'large',
+            },
+            items: [{
+                cls: 'slate-cbl-ratings-rater-menu-btn',
+                glyph: 'xf0c9', // fa-bars
+                menu: {
+                    items: [{
+                        text: '1',
+                    }, {
+                        text: '2',
+                    }, {
+                        text: '3',
+                    }, {
+                        text: '4',
+                    }, {
+                        text: 'DNM',
+                    }],
+                },
+            }, {
+                text: 'M',
+            }, {
+                text: '5',
+            }, {
+                text: '6',
+            }, {
+                text: '7',
+            }, {
+                text: '8',
+            }, {
+                text: '9',
+            }, {
+                text: '10',
+            }, {
+                text: '11',
+            }, {
+                text: '12',
+            }, {
+                glyph: 'xf00c', // fa-check
+            }],
+        }],
+    }],
+
+    // config handlers
+    // applySkill: function(skill, oldSkill) {
+    //     if (!skill) {
+    //         return null;
+    //     }
+
+    //     if (!skill.isModel) {
+    //         if (oldSkill && skill.ID == oldSkill.getId()) {
+    //             oldSkill.set(skill, { dirty: false });
+    //             return oldSkill;
+    //         }
+
+    //         skill = Slate.cbl.model.Skill.create(skill);
+    //     }
+
+    //     return skill;
+    // },
+
+    // updateSkill: function(skill) {
+    //     this.setFieldLabel(skill ? skill.get('Code') + '&mdash;' + skill.get('Descriptor') : null);
+    // },
+});
