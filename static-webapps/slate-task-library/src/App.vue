ce2422ef6d747b4116c7e7b682da84e2ddf371af
Chef Habitat Studio <chef-habitat@studio>
2023-02-10 09:24:15 +0000

    feat: remove boilerplate

--- static-webapps/slate-task-library/src/App.vue
+++ static-webapps/slate-task-library/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+    <h1>test</h1>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
