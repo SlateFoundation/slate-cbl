41e5e4bb3a527eb6d963380a5a8ee4072e523847
BillClinton <bclinton@appunto.net>
2023-02-12 00:14:49 -0500

    feat: add data table to vuetify plugin
    Currently pulling this from labs until it is included in
    main release

--- static-webapps/slate-task-library/src/plugins/vuetify.js
+++ static-webapps/slate-task-library/src/plugins/vuetify.js
@@ -11,8 +11,15 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+// Components
+import { VDataTable } from 'vuetify/labs/VDataTable'
+
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: {
+    VDataTable
+  },
   theme: {
     themes: {
       light: {
