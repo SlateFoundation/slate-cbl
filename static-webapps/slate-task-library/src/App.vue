e3cf9dc3e5d747dfcd0c89b9c2b58072d65cfb99
BillClinton <bclinton@appunto.net>
2023-02-12 00:19:20 -0500

    feat: add task data table w/ test data

--- static-webapps/slate-task-library/src/App.vue
+++ static-webapps/slate-task-library/src/App.vue
@@ -2,9 +2,11 @@
   <v-app>
     <v-main>
     <h1>test</h1>
+    <MyDataTable />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import MyDataTable from './components/MyDataTable.vue'
 </script>
