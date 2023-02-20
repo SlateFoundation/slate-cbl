c5c3e854d19a3f29b942dd9d1febbb35d299ec5c
BillClinton <bclinton@appunto.net>
2023-02-20 14:49:05 -0500

    feat: add delete confirmation dialog

---
+++ static-webapps/slate-task-library/src/components/DeleteConfirmation.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-dialog v-model="dialog" persistent width="auto">
+    <template #activator="{ props }">
+      <v-btn
+        :disabled="task === null"
+        icon="mdi-trash-can-outline"
+        color="primary"
+        v-bind="props"
+      />
+    </template>
+    <v-card>
+      <v-card-title class="text-h5"> Delete Task </v-card-title>
+      <v-card-text>{{ task.raw.Title }}</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green-darken-1" variant="text" @click="dialog = false">
+          Delete
+        </v-btn>
+        <v-btn color="green-darken-1" variant="text" @click="dialog = false">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    task: Object,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style>
+button {
+  margin-left: 1em;
+}
+</style>
--- static-webapps/slate-task-library/src/components/TasksHeader.vue
+++ static-webapps/slate-task-library/src/components/TasksHeader.vue
@@ -3,28 +3,21 @@
     <v-col cols="12" sm="8">
       <h1>Task Library</h1>
     </v-col>
-    <v-col cols="12" sm="2" align="right">
+    <v-col cols="12" sm="2">
       <v-btn icon="mdi-plus" color="primary" />
       <v-btn :disabled="task === null" icon="mdi-pencil" color="primary" />
-      <v-btn
-        :disabled="task === null"
-        icon="mdi-trash-can-outline"
-        color="primary"
-      />
+      <DeleteConfirmation :task="task" />
     </v-col>
   </v-row>
 </template>
 
 <script>
+import DeleteConfirmation from "./DeleteConfirmation.vue";
+
 export default {
+  components: { DeleteConfirmation },
   props: {
     task: Object,
   },
 };
 </script>
-
-<style>
-button {
-  margin-left: 1em;
-}
-</style>
