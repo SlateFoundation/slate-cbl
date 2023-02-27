cc89761429e44cded53ac652478a8af86cd13f06
BillClinton <bclinton@appunto.net>
2023-02-27 03:48:20 -0500

    refactor: remove unnecessary transformData action
    this is taken care of by the item-title and item-value props
    in the v-autocomplete component

--- static-webapps/slate-task-library/src/stores/ExperienceTypeStore.js
+++ static-webapps/slate-task-library/src/stores/ExperienceTypeStore.js
@@ -4,9 +4,4 @@ export const useExperienceTypeStore = defineStore("experienceTypeStore", {
   state: () => ({
     path: "/cbl/tasks/*experience-types",
   }),
-  actions: {
-    transformData(data) {
-      return data.map((item, index) => ({ title: item, value: index }));
-    },
-  },
 });
