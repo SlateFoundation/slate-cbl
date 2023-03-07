54a223b45b55eff3236ac6da159b7e2ce3c67503
BillClinton <bclinton@appunto.net>
2023-03-07 17:20:09 -0500

    feat: add logging of hierarchical state values

--- static-webapps/slate-task-library/src/machines/logTransition.js
+++ static-webapps/slate-task-library/src/machines/logTransition.js
@@ -1,7 +1,12 @@
 const logTransition = (state) => {
   if (state.context.logTransition) {
+    let current = state.value;
+
+    if (typeof current === "object") {
+      current = JSON.stringify(current);
+    }
     console.groupCollapsed(
-      `[#] <${state.machine.id}> transitioned to ${state.value}`
+      `[#] <${state.machine.id}> transitioned to ${current}`
     );
     console.log("event: ", state.event);
     console.log("state: ", state);
