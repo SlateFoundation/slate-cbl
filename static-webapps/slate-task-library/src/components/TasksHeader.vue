52642d74ad223c74ab6eae53031ccfb611d79a88
BillClinton <bclinton@appunto.net>
2023-03-07 00:48:37 -0500

    refactor: use preferred syntax

--- static-webapps/slate-task-library/src/components/TasksHeader.vue
+++ static-webapps/slate-task-library/src/components/TasksHeader.vue
@@ -3,12 +3,12 @@
     <h1>Task Library</h1>
   </v-col>
   <v-col cols="12" sm="2">
-    <v-btn icon="mdi-plus" color="primary" @click="send('ADD')" />
+    <v-btn icon="mdi-plus" color="primary" @click="send({ type: 'ADD' })" />
     <v-btn
       :disabled="task === null"
       icon="mdi-pencil"
       color="primary"
-      @click="send('EDIT', task)"
+      @click="send({ type: 'EDIT' })"
     />
     <DeleteConfirmation :task="task" />
   </v-col>
